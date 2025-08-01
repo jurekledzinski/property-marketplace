c3003aa1459fc6ebc3a2ebe8d0ea9e05bead8921
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-01 23:45:40 +0200

    Add test inputs to filter panel

--- components/pages/home/components/filter-panel/FilterPanel.tsx
+++ components/pages/home/components/filter-panel/FilterPanel.tsx
@@ -1,3 +1,32 @@
+'use client';
+
+import styles from './FilterPanel.module.css';
+
 export const FilterPanel = () => {
-  return <div></div>;
+  return (
+    <div className={styles.panel}>
+      <div className={styles.group}>
+        <button>Clear all</button>
+        <button>Apply</button>
+      </div>
+      <div className={styles.filters}>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input a</p>
+      </div>
+    </div>
+  );
 };
