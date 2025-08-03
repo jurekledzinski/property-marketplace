2e6a0ab83b371e8df0fd1ff97cbb80991e21c80c
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-03 21:49:57 +0200

    Provide form controls to login form

--- components/pages/login/Login.tsx
+++ components/pages/login/Login.tsx
@@ -1,9 +1,22 @@
 import styles from './Login.module.css';
 import stylesCommon from './Common.module.css';
-import { Box, Heading, LoginForm } from '@/components';
 import { classNames } from '@/helpers';
+import { SubmitHandler } from 'react-hook-form';
+import {
+  Box,
+  Heading,
+  InputsLogin,
+  LoginForm,
+  useLoginForm,
+} from '@/components';
 
 export const LoginPage = () => {
+  const controls = useLoginForm();
+
+  const onSubmit: SubmitHandler<InputsLogin> = (data) => {
+    console.log('Submit', data);
+  };
+
   return (
     <Box className={stylesCommon.container}>
       <Box className={stylesCommon.wrapper}>
@@ -14,7 +27,7 @@ export const LoginPage = () => {
           <Heading className={stylesCommon.title} level={4}>
             Sign in
           </Heading>
-          <LoginForm />
+          <LoginForm controls={controls} onSubmit={onSubmit} />
         </Box>
       </Box>
     </Box>
