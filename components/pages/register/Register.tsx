593a5d95a1d25754fb131300ee0789f1c665d9ba
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-03 17:23:19 +0200

    Update register page

--- components/pages/register/Register.tsx
+++ components/pages/register/Register.tsx
@@ -1,17 +1,23 @@
+'use client';
 import styles from './Register.module.css';
-import { Heading } from '@/components';
+import { Box, Heading, RegisterForm } from '@/components';
+import { useControlRegisterForm, usePasswordRules } from './hooks';
 
 export const RegisterPage = () => {
+  const controls = useControlRegisterForm();
+  const passwordRules = usePasswordRules({ watch: controls.watch });
+
   return (
-    <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <div className={styles.left}></div>
-        <div className={styles.right}>
+    <Box className={styles.container}>
+      <Box className={styles.wrapper}>
+        <Box className={styles.left}></Box>
+        <Box className={styles.right}>
           <Heading className={styles.title} level={4}>
             Sign up
           </Heading>
-        </div>
-      </div>
-    </div>
+          <RegisterForm controls={controls} passwordRules={passwordRules} />
+        </Box>
+      </Box>
+    </Box>
   );
 };
