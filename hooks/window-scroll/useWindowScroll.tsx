1345e91536031dbb6e6e8f8704a9bb7dbc3edcdd
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-03 14:00:56 +0200

    Create custom hook window scroll

---
+++ hooks/window-scroll/useWindowScroll.tsx
@@ -0,0 +1,8 @@
+import { useEffect } from 'react';
+import { UseWindowScroll } from './types';
+
+export const useWindowScroll = ({ onScroll }: UseWindowScroll) => {
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll, true);
+  }, [onScroll]);
+};
