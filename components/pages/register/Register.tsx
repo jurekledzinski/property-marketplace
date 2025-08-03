754838bc65a46fc4027ff54783ac07fd92b44ae0
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-03 20:55:05 +0200

    Add custom hooks use register form and password rules and pass they data as props to register form

--- components/pages/register/Register.tsx
+++ components/pages/register/Register.tsx
@@ -1,11 +1,15 @@
 'use client';
 import styles from './Register.module.css';
 import { Box, Heading, RegisterForm } from '@/components';
-import { useControlRegisterForm, usePasswordRules } from './hooks';
+import { SubmitHandler } from 'react-hook-form';
+import { InputsRegister, useRegisterForm, usePasswordRules } from './hooks';
 
 export const RegisterPage = () => {
-  const controls = useControlRegisterForm();
+  const controls = useRegisterForm();
   const passwordRules = usePasswordRules({ watch: controls.watch });
+  const onSubmit: SubmitHandler<InputsRegister> = (data) => {
+    console.log('Submit', data);
+  };
 
   return (
     <Box className={styles.container}>
@@ -15,7 +19,11 @@ export const RegisterPage = () => {
           <Heading className={styles.title} level={4}>
             Sign up
           </Heading>
-          <RegisterForm controls={controls} passwordRules={passwordRules} />
+          <RegisterForm
+            controls={controls}
+            passwordRules={passwordRules}
+            onSubmit={onSubmit}
+          />
         </Box>
       </Box>
     </Box>
