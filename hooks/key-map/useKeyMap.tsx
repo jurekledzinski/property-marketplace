4ab5acf218a5a45ca0b7836b56a998e30e4e2601
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-03 14:00:05 +0200

    Create custom hook to create keys map

---
+++ hooks/key-map/useKeyMap.tsx
@@ -0,0 +1,15 @@
+import { useRef } from 'react';
+
+export const useKeyMap = <T extends HTMLElement | string>() => {
+  const mapKeys = useRef(new Map<string, T>());
+
+  const onSetKey = (id: string, ref: T) => {
+    mapKeys.current.set(id, ref);
+  };
+
+  const onDeleteKey = (id: string) => {
+    mapKeys.current.delete(id);
+  };
+
+  return { mapKeys, onDeleteKey, onSetKey };
+};
