07cb6334c88c1d717412644424210f9632684c5b
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-03 17:21:55 +0200

    Work on login page

--- components/pages/login/Login.tsx
+++ components/pages/login/Login.tsx
@@ -1,17 +1,18 @@
 import styles from './Login.module.css';
-import { Heading } from '@/components';
+import { Box, Heading, LoginForm } from '@/components';
 
 export const LoginPage = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <div className={styles.left}></div>
-        <div className={styles.right}>
+    <Box className={styles.container}>
+      <Box className={styles.wrapper}>
+        <Box className={styles.left}></Box>
+        <Box className={styles.right}>
           <Heading className={styles.title} level={4}>
             Sign in
           </Heading>
-        </div>
-      </div>
-    </div>
+          <LoginForm />
+        </Box>
+      </Box>
+    </Box>
   );
 };
