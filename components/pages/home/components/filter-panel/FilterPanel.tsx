4f7ef8b36892e7aabed567222f5b5214c96f5744
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-04 16:16:11 +0200

    Replace html buttons with button components in filter panel

--- components/pages/home/components/filter-panel/FilterPanel.tsx
+++ components/pages/home/components/filter-panel/FilterPanel.tsx
@@ -1,13 +1,14 @@
 'use client';
 import styles from './FilterPanel.module.css';
 import { FilterPanelProps } from './types';
+import { Button } from '@/components';
 
 export const FilterPanel = ({ children }: FilterPanelProps) => {
   return (
     <div className={styles.panel}>
       <div className={styles.group}>
-        <button>Clear all</button>
-        <button>Apply</button>
+        <Button label="Clear all" color="secondary" variant="outlined" />
+        <Button label="Apply" color="success" />
       </div>
       <div className={styles.filters}>{children}</div>
     </div>
