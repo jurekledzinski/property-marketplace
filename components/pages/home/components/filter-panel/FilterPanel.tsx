3608fc7d0ab257134b38209f498cd0ba06f0152d
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-04 17:25:44 +0200

    Add attributes to buttons to choose styling

--- components/pages/home/components/filter-panel/FilterPanel.tsx
+++ components/pages/home/components/filter-panel/FilterPanel.tsx
@@ -7,8 +7,13 @@ export const FilterPanel = ({ children }: FilterPanelProps) => {
   return (
     <div className={styles.panel}>
       <div className={styles.group}>
-        <Button label="Clear all" color="secondary" variant="outlined" />
-        <Button label="Apply" color="success" />
+        <Button
+          label="Clear all"
+          color="secondary"
+          variant="outlined"
+          fullWidth
+        />
+        <Button label="Apply" color="success" fullWidth />
       </div>
       <div className={styles.filters}>{children}</div>
     </div>
