34cdaea80e55e35448e0bdbf825862e252b7acde
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-04 14:56:11 +0200

    Work on filter panel component

--- components/pages/home/components/filter-panel/FilterPanel.tsx
+++ components/pages/home/components/filter-panel/FilterPanel.tsx
@@ -1,32 +1,32 @@
 'use client';
-
 import styles from './FilterPanel.module.css';
+import { FilterPanelProps } from './types';
 
-export const FilterPanel = () => {
+export const FilterPanel = ({ children }: FilterPanelProps) => {
   return (
     <div className={styles.panel}>
       <div className={styles.group}>
         <button>Clear all</button>
         <button>Apply</button>
       </div>
-      <div className={styles.filters}>
-        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
-        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
-        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
-        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
-        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
-        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
-        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
-        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
-        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
-        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
-        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
-        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
-        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
-        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
-        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
-        <p style={{ padding: 8, border: '1px solid black' }}>input a</p>
-      </div>
+      <div className={styles.filters}>{children}</div>
     </div>
   );
 };
+
+/* <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input</p>
+        <p style={{ padding: 8, border: '1px solid black' }}>input a</p> */
--- components/pages/home/components/filter-panel/index.ts
+++ components/pages/home/components/filter-panel/index.ts
@@ -1,1 +1,3 @@
+export * from './components';
 export * from './FilterPanel';
+export * from './types';
