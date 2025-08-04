3eab7ef3e80942f22cc2e124f51e650b10e82d5f
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-04 14:57:20 +0200

    Create custom hook to control filter form

---
+++ components/pages/home/hooks/control-filter-form/types.ts
@@ -0,0 +1,13 @@
+export type InputsAdvertsFilter = {
+  country: string;
+  city: string;
+  property: string;
+  condition: string;
+  advertisement: string;
+  year: string;
+  price: string;
+  area: string;
+  rooms: string;
+  amenities: string[];
+  style: string;
+};
---
+++ components/pages/home/hooks/control-filter-form/useFilterForm.tsx
@@ -0,0 +1,24 @@
+import { InputsAdvertsFilter } from './types';
+import { useForm } from 'react-hook-form';
+
+const defaultValues = {
+  country: '',
+  city: '',
+  property: '',
+  condition: '',
+  advertisement: '',
+  year: '',
+  price: '',
+  area: '',
+  rooms: '',
+  amenities: [],
+  style: '',
+};
+
+export const useFilterForm = () => {
+  const formControl = useForm<InputsAdvertsFilter>({
+    defaultValues,
+  });
+
+  return formControl;
+};
