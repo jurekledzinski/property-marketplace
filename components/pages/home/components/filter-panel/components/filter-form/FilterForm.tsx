559cc26e04ee65ededf9fc87884bc0b5400cbadc
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-05 17:55:08 +0200

    Update form filter component and memoize it

--- components/pages/home/components/filter-panel/components/filter-form/FilterForm.tsx
+++ components/pages/home/components/filter-panel/components/filter-form/FilterForm.tsx
@@ -1,21 +1,26 @@
 import { FilterFormProps } from './types';
+import { memo } from 'react';
 import {
   AmenitiesSection,
   ArchitectureSection,
   Form,
   LocationSection,
+  PriceSection,
   PropertyDetailsSection,
   PropertyTypeSection,
 } from '@/components';
 
-export const FilterForm = ({ controls, onSubmit }: FilterFormProps) => {
+export const FilterForm = memo(({ controls, onSubmit }: FilterFormProps) => {
   return (
-    <Form onSubmit={controls.handleSubmit(onSubmit)} noValidate>
+    <Form id="filterForm" onSubmit={controls.handleSubmit(onSubmit)} noValidate>
       <LocationSection controls={controls} />
       <PropertyTypeSection controls={controls} />
       <PropertyDetailsSection controls={controls} />
+      <PriceSection controls={controls} />
       <AmenitiesSection controls={controls} />
       <ArchitectureSection controls={controls} />
     </Form>
   );
-};
+});
+
+FilterForm.displayName = 'FilterForm';
