c406f53a940ffb80e3dab1e68d15e3e5a351c183
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-05 17:56:05 +0200

    Add use client directive to useFilterForm custom hook

--- components/pages/home/hooks/control-filter-form/useFilterForm.tsx
+++ components/pages/home/hooks/control-filter-form/useFilterForm.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { InputsAdvertsFilter } from './types';
 import { useForm } from 'react-hook-form';
 
@@ -8,7 +9,8 @@ const defaultValues = {
   condition: '',
   advertisement: '',
   year: '',
-  price: '',
+  priceFrom: '',
+  priceTo: '',
   area: '',
   rooms: '',
   amenities: [],
