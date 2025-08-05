0fdf0ed958d4d7818a12565a25ba793c4afb843d
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-05 18:54:24 +0200

    Refactor property details section component separate it from prices that later can be reuse

--- components/shared/property-form-sections/property-details-section/PropertyDetailsSection.tsx
+++ components/shared/property-form-sections/property-details-section/PropertyDetailsSection.tsx
@@ -1,95 +1,38 @@
 'use client';
-import { Controller } from 'react-hook-form';
 import { Field, TextInput } from '@/components';
 import { PropertyDetailsSectionProps } from './types';
-import { useRef } from 'react';
 
 export const PropertyDetailsSection = ({
   controls,
 }: PropertyDetailsSectionProps) => {
-  const { control, setValue } = controls;
-  const type = useRef<'number' | 'text'>('number');
+  const { register } = controls;
 
   return (
     <>
       <Field>
         <TextInput
-          {...control.register('year')}
+          {...register('year')}
           type="number"
           min={1800}
           max={new Date().getFullYear()}
           placeholder="Year of construciton"
         />
       </Field>
-
-      <Field>
-        <Controller
-          name="priceFrom"
-          control={control}
-          render={({ field: { ...rest } }) => (
-            <TextInput
-              {...rest}
-              placeholder="Price from"
-              type={type.current}
-              onFocus={(e) => {
-                type.current = 'number';
-                const value = e.target.value.replace(/[^\d]/g, '');
-                setValue('priceFrom', value);
-              }}
-              onBlur={(e) => {
-                type.current = 'text';
-                // const value = e.target.value;
-                // const format = formatNumber(value, 'nl-NL', optionsFormat);
-                // setValue('priceFrom', format.format);
-              }}
-              min={0}
-            />
-          )}
-        />
-      </Field>
-
-      <Field>
-        <Controller
-          name="priceTo"
-          control={control}
-          render={({ field: { ...rest } }) => (
-            <TextInput
-              {...rest}
-              placeholder="Price to"
-              type={type.current}
-              onFocus={(e) => {
-                type.current = 'number';
-                const value = e.target.value.replace(/[^\d]/g, '');
-                setValue('priceTo', value);
-              }}
-              onBlur={(e) => {
-                type.current = 'text';
-                // const value = e.target.value;
-                // const format = formatNumber(value, 'nl-NL', optionsFormat);
-                // setValue('priceFrom', format.format);
-              }}
-              min={0}
-            />
-          )}
-        />
-      </Field>
-
       <Field>
         <TextInput
-          {...control.register('area')}
+          {...register('area')}
           placeholder="Area"
           type="number"
           endIcon={['m²']}
-          min={0}
+          min={1}
         />
       </Field>
-
       <Field>
         <TextInput
-          {...control.register('rooms')}
+          {...register('rooms')}
           placeholder="Number of rooms"
           type="number"
-          min={0}
+          min={1}
           step={1}
         />
       </Field>
