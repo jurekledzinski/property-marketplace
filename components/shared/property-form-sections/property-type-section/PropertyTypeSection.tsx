2301bf8b16da10d811688266bccd3b554cad0de5
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-05 18:54:46 +0200

    Refactor property type section component

--- components/shared/property-form-sections/property-type-section/PropertyTypeSection.tsx
+++ components/shared/property-form-sections/property-type-section/PropertyTypeSection.tsx
@@ -1,7 +1,9 @@
+'use client';
 import { Controller } from 'react-hook-form';
 import { PropertyTypeSectionProps } from './types';
 import {
   Field,
+  Message,
   Select,
   SelectList,
   SelectOption,
@@ -9,14 +11,32 @@ import {
   SelectTrigger,
 } from '@/components';
 
-export const PropertyTypeSection = ({ controls }: PropertyTypeSectionProps) => {
-  const { control } = controls;
+const conditons = [
+  'New',
+  'Like New',
+  'Renovated',
+  'Good Condition',
+  'Needs Renovation',
+  'Under Construction',
+  'Old',
+  'Not Specified',
+];
+
+export const PropertyTypeSection = ({
+  controls,
+  rulesAdvertisment,
+  rulesCondition,
+  rulesProperty,
+}: PropertyTypeSectionProps) => {
+  const { control, formState } = controls;
+  const { errors } = formState;
 
   return (
     <>
       <Field>
         <Controller
           name={'property'}
+          rules={rulesProperty}
           control={control}
           render={({ field: { onChange, ...rest } }) => (
             <Select onChange={(id) => onChange(id)} {...rest}>
@@ -30,34 +50,38 @@ export const PropertyTypeSection = ({ controls }: PropertyTypeSectionProps) => {
             </Select>
           )}
         />
+        {errors.property ? (
+          <Message variant="error">{errors.property?.message}</Message>
+        ) : null}
       </Field>
       <Field>
         <Controller
           name={'condition'}
+          rules={rulesCondition}
           control={control}
           render={({ field: { onChange, ...rest } }) => (
             <Select onChange={(id) => onChange(id)} {...rest}>
               <SelectTrigger placeholder="Select technical condition" />
               <SelectPanel>
                 <SelectList>
-                  <SelectOption id="new">New</SelectOption>
-                  <SelectOption id="old">Old</SelectOption>
-                  <SelectOption id="renovated">Renovated</SelectOption>
-                  <SelectOption id="needs renovation">
-                    Needs Renovation
-                  </SelectOption>
-                  <SelectOption id="under construction">
-                    Under Construction
-                  </SelectOption>
+                  {conditons.map((condition) => (
+                    <SelectOption key={condition} id={condition.toLowerCase()}>
+                      {condition}
+                    </SelectOption>
+                  ))}
                 </SelectList>
               </SelectPanel>
             </Select>
           )}
         />
+        {errors.condition ? (
+          <Message variant="error">{errors.condition?.message}</Message>
+        ) : null}
       </Field>
       <Field>
         <Controller
           name={'advertisement'}
+          rules={rulesAdvertisment}
           control={control}
           render={({ field: { onChange, ...rest } }) => (
             <Select onChange={(id) => onChange(id)} {...rest}>
@@ -71,6 +95,9 @@ export const PropertyTypeSection = ({ controls }: PropertyTypeSectionProps) => {
             </Select>
           )}
         />
+        {errors.advertisement ? (
+          <Message variant="error">{errors.advertisement?.message}</Message>
+        ) : null}
       </Field>
     </>
   );
