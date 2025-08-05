78d96819ae93a306d80fcf1395feab002842ad04
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-05 18:55:15 +0200

    Add rules types props in PropertyTypeSection component

--- components/shared/property-form-sections/property-type-section/types.ts
+++ components/shared/property-form-sections/property-type-section/types.ts
@@ -1,6 +1,14 @@
-import { InputsAdvertsFilter } from '@/components/pages/home/hooks';
-import { UseFormReturn } from 'react-hook-form';
+import { InputsAdvertsFilter } from '@/components';
+import { UseControllerProps, UseFormReturn } from 'react-hook-form';
+
+type Rule<K extends keyof InputsAdvertsFilter> = UseControllerProps<
+  InputsAdvertsFilter,
+  K
+>['rules'];
 
 export type PropertyTypeSectionProps = {
   controls: UseFormReturn<InputsAdvertsFilter, unknown, InputsAdvertsFilter>;
+  rulesProperty?: Rule<'property'>;
+  rulesCondition?: Rule<'condition'>;
+  rulesAdvertisment?: Rule<'advertisement'>;
 };
