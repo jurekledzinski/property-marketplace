7ced0968666b798e746aca998f90a489afc4633c
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-05 17:52:59 +0200

    Create separate header panel and cards sections components from content in advert layout component

--- components/pages/home/components/adverts-layout/AdvertsLayout.tsx
+++ components/pages/home/components/adverts-layout/AdvertsLayout.tsx
@@ -1,37 +1,27 @@
-import Image from 'next/image';
 import styles from './AdvertsLayout.module.css';
-import { Backdrop, Drawer, FilterForm, FilterPanel } from '@/components';
-import { InputsAdvertsFilter, useFilterForm } from '../../hooks';
+import { InputsAdvertsFilter, useFilterForm } from '@/components';
 import { SubmitHandler } from 'react-hook-form';
+import { useCallback } from 'react';
+import {
+  Backdrop,
+  Drawer,
+  FilterForm,
+  FilterPanel,
+  HeaderPanel,
+  CardsSection,
+} from '@/components';
 
 export const AdvertsLayout = () => {
   const controls = useFilterForm();
-  const onSubmit: SubmitHandler<InputsAdvertsFilter> = (data) => {
+  const onSubmit: SubmitHandler<InputsAdvertsFilter> = useCallback((data) => {
     console.log('Submit', data);
-  };
+  }, []);
 
   return (
     <div className={styles.layout}>
       <div className={styles.wrapper}>
-        <header className={styles.header}>
-          <input type="text" placeholder="Search" />
-          <input type="text" placeholder="Sort" />
-        </header>
-        <section className={styles.section}>
-          {Array.from({ length: 10 }).map((_, index) => {
-            return (
-              <div key={index} className={styles.box}>
-                <Image
-                  src="https://cdn.pixabay.com/photo/2017/06/21/19/04/farm-2428245_1280.jpg"
-                  alt="Beach"
-                  className={styles.image}
-                  width={858}
-                  height={400}
-                />
-              </div>
-            );
-          })}
-        </section>
+        <HeaderPanel />
+        <CardsSection />
       </div>
       <Backdrop open={false} />
       <Drawer variant="pinned" direction="right">
