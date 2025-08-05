2b910849fe7d2bb05b7acb61e3eb73261f368782
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-05 19:19:20 +0200

    Correct types and errors PropertyDetailsSection

--- components/shared/property-form-sections/property-details-section/PropertyDetailsSection.tsx
+++ components/shared/property-form-sections/property-details-section/PropertyDetailsSection.tsx
@@ -1,40 +1,53 @@
 'use client';
-import { Field, TextInput } from '@/components';
+import { Field, Message, TextInput } from '@/components';
 import { PropertyDetailsSectionProps } from './types';
 
 export const PropertyDetailsSection = ({
   controls,
+  rulesArea,
+  rulesRooms,
+  rulesYear,
 }: PropertyDetailsSectionProps) => {
-  const { register } = controls;
+  const { formState, register } = controls;
+  const { errors } = formState;
 
   return (
     <>
       <Field>
         <TextInput
-          {...register('year')}
+          {...register('year', rulesYear)}
           type="number"
           min={1800}
           max={new Date().getFullYear()}
           placeholder="Year of construciton"
         />
+        {errors.year ? (
+          <Message variant="error">{errors.year.message}</Message>
+        ) : null}
       </Field>
       <Field>
         <TextInput
-          {...register('area')}
+          {...register('area', rulesArea)}
           placeholder="Area"
           type="number"
           endIcon={['m²']}
           min={1}
         />
+        {errors.area ? (
+          <Message variant="error">{errors.area.message}</Message>
+        ) : null}
       </Field>
       <Field>
         <TextInput
-          {...register('rooms')}
+          {...register('rooms', rulesRooms)}
           placeholder="Number of rooms"
           type="number"
           min={1}
           step={1}
         />
+        {errors.rooms ? (
+          <Message variant="error">{errors.rooms.message}</Message>
+        ) : null}
       </Field>
     </>
   );
--- components/shared/property-form-sections/property-details-section/types.ts
+++ components/shared/property-form-sections/property-details-section/types.ts
@@ -1,6 +1,10 @@
 import { InputsAdvertsFilter } from '@/components';
+import { Rule } from '../types';
 import { UseFormReturn } from 'react-hook-form';
 
 export type PropertyDetailsSectionProps = {
   controls: UseFormReturn<InputsAdvertsFilter, unknown, InputsAdvertsFilter>;
+  rulesArea?: Rule<'area'>;
+  rulesRooms?: Rule<'rooms'>;
+  rulesYear?: Rule<'year'>;
 };
