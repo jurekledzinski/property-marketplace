24a60c6b48a8c6ad72c20b82f05a397a01c61e94
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-07 13:45:23 +0200

    Refactor CardSection component

--- components/pages/home/components/cards-section/CardsSection.tsx
+++ components/pages/home/components/cards-section/CardsSection.tsx
@@ -1,23 +1,82 @@
-import Image from 'next/image';
 import styles from './CardsSection.module.css';
 import { memo } from 'react';
+
+import { CardAdvert } from './components';
+
+const data = [
+  {
+    area: 100,
+    bathrooms: 1,
+    city: 'Szczecin',
+    country: 'Poland',
+    postalCode: '50-490',
+    price: 120000,
+    rooms: 3,
+    street: 'Akwarela 5',
+    title: 'Modern  big and nice house',
+    typeAdvert: 'Sale',
+    src: 'https://cdn.pixabay.com/photo/2025/06/10/11/21/view-9651981_1280.jpg',
+  },
+  {
+    area: 100,
+    bathrooms: 1,
+    city: 'Szczecin',
+    country: 'Poland',
+    postalCode: '50-490',
+    price: 120000,
+    rooms: 3,
+    street: 'Akwarela 5',
+    title: 'Tiny house',
+    typeAdvert: 'Sale',
+    src: 'https://cdn.pixabay.com/photo/2017/05/28/18/48/ivy-2351778_1280.jpg',
+  },
+  {
+    area: 100,
+    bathrooms: 1,
+    city: 'Szczecin',
+    country: 'Poland',
+    postalCode: '50-490',
+    price: 120000,
+    rooms: 3,
+    street: 'Akwarela 5',
+    title: 'Tiny house',
+    typeAdvert: 'Sale',
+    src: 'https://cdn.pixabay.com/photo/2017/05/31/10/23/manor-house-2359884_1280.jpg',
+  },
+  {
+    area: 100,
+    bathrooms: 1,
+    city: 'Szczecin',
+    country: 'Poland',
+    postalCode: '50-490',
+    price: 120000,
+    rooms: 3,
+    street: 'Akwarela 5',
+    title: 'Tiny house',
+    typeAdvert: 'Sale',
+    src: 'https://cdn.pixabay.com/photo/2017/06/21/16/33/farm-2427751_1280.jpg',
+  },
+  {
+    area: 100,
+    bathrooms: 1,
+    city: 'Szczecin',
+    country: 'Poland',
+    postalCode: '50-490',
+    price: 120000,
+    rooms: 3,
+    street: 'Akwarela 5',
+    title: 'Tiny house',
+    typeAdvert: 'Sale',
+    src: 'https://images.pexels.com/photos/3774243/pexels-photo-3774243.jpeg',
+  },
+];
 
 export const CardsSection = memo(() => {
   return (
     <section className={styles.section}>
-      {Array.from({ length: 10 }).map((_, index) => {
-        return (
-          <div key={index} className={styles.box}>
-            <Image
-              src="https://cdn.pixabay.com/photo/2017/06/21/19/04/farm-2428245_1280.jpg"
-              alt="Beach"
-              className={styles.image}
-              width={858}
-              height={400}
-            />
-          </div>
-        );
-      })}
+      {data.map((advert, index) => (
+        <CardAdvert key={index} dataAdvert={advert} />
+      ))}
     </section>
   );
 });
