9414847948a40e5c1f87d4933b2dafbc855be68d
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-07 13:54:31 +0200

    Add className prop to possibility add extra styles

--- components/shared/image/Image.tsx
+++ components/shared/image/Image.tsx
@@ -1,7 +1,10 @@
+import ImageNext from 'next/image';
 import styles from './Image.module.css';
 import { ImageProps } from './types';
-import ImageNext from 'next/image';
+import { classNames } from '@/helpers';
 
-export const Image = ({ ...props }: ImageProps) => {
-  return <ImageNext {...props} className={styles.image} />;
+export const Image = ({ className, ...props }: ImageProps) => {
+  return (
+    <ImageNext {...props} className={classNames(className, styles.image)} />
+  );
 };
