7be73c116e255a21fafd79274e54b787cb161d7b
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-08 11:40:20 +0200

    Change order spread props in input checkbox

--- components/shared/checkbox/components/checkbox-input/CheckboxInput.tsx
+++ components/shared/checkbox/components/checkbox-input/CheckboxInput.tsx
@@ -1,25 +1,22 @@
 import { CheckboxProps } from '../../types';
-import { forwardRef, Ref } from 'react';
+import { forwardRef } from 'react';
 import { getClassCheckbox } from '../../utils';
 import { Icon } from '../icon';
 
 export const CheckboxInput = forwardRef<HTMLInputElement, CheckboxProps>(
-  (
-    { color, disabled, readOnly, size, variant, ...props },
-    ref: Ref<HTMLInputElement>
-  ) => {
+  ({ color, disabled, readOnly, size, variant, ...props }, ref) => {
     const classes = getClassCheckbox(color, disabled, readOnly, size, variant);
 
     return (
       <>
         <input
+          {...props}
           ref={ref}
           className={classes.checkbox}
           disabled={disabled}
           id={props.id}
           readOnly={readOnly}
           type="checkbox"
-          {...props}
           onKeyDown={(e) => readOnly && e.preventDefault()}
         />
         <Icon className={classes.checkbox} />
