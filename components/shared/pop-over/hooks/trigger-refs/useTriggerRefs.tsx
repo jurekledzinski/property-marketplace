bb0ba1087679362a31a2bd005ddda9e8ac9c894a
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-10 14:23:48 +0200

    Refactor useTriggerRefs hook add map for triggerRects

--- components/shared/pop-over/hooks/trigger-refs/useTriggerRefs.tsx
+++ components/shared/pop-over/hooks/trigger-refs/useTriggerRefs.tsx
@@ -3,14 +3,15 @@ import { useCallback, useRef } from 'react';
 
 export const useTriggerRefs = () => {
   const triggerRefs = useRef<Record<string, HTMLElement | null>>({});
+  const triggerRects = useRef<Record<string, DOMRect>>({});
 
   const registerTriggerRef = useCallback(
     (node: HTMLElement | null, id: string) => {
       if (!id) return;
-      triggerRefs.current[id] = node;
 
       if (node) {
         triggerRefs.current[id] = node;
+        triggerRects.current[id] = node.getBoundingClientRect();
       } else {
         delete triggerRefs.current[id];
       }
@@ -18,5 +19,21 @@ export const useTriggerRefs = () => {
     []
   );
 
-  return { triggerRefs, registerTriggerRef };
+  const getTriggerRect = (id: string) => {
+    return triggerRects.current[id];
+  };
+
+  const refreshTriggerRect = (id: string) => {
+    if (!triggerRefs.current || !triggerRefs.current[id]) return;
+
+    const rect = triggerRefs.current[id].getBoundingClientRect();
+    triggerRects.current[id] = rect;
+  };
+
+  return {
+    getTriggerRect,
+    refreshTriggerRect,
+    registerTriggerRef,
+    triggerRefs,
+  };
 };
