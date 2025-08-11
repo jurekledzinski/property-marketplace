c1a1d9a25bc9e0d7a95033506735d46a161f6819
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-11 19:44:47 +0200

    Display chips in filters chips panel

--- components/pages/home/components/filters-chips-panel/FiltersChipsPanel.tsx
+++ components/pages/home/components/filters-chips-panel/FiltersChipsPanel.tsx
@@ -1,6 +1,43 @@
 import styles from './FiltersChipsPanel.module.css';
+import { Chip } from '@/components';
 import { FiltersChipsPanelProps } from './types';
+import { capitalizeFirstLetter } from '@/helpers';
 
-export const FiltersChipsPanel = ({ children }: FiltersChipsPanelProps) => {
-  return <div className={styles.chipBar}>{children}</div>;
+export const FiltersChipsPanel = ({
+  filters,
+  onDelete,
+}: FiltersChipsPanelProps) => {
+  return (
+    <div className={styles.chipBar}>
+      {Object.entries(filters).map((filter) => {
+        if (Array.isArray(filter[1])) {
+          return filter[1].map((amenity) => {
+            return (
+              <Chip
+                name={filter[0]}
+                id={amenity}
+                key={amenity}
+                label={`Amenity: ${capitalizeFirstLetter(amenity)}`}
+                color="success"
+                onDelete={onDelete}
+              />
+            );
+          });
+        } else {
+          return (
+            <Chip
+              name={filter[0]}
+              id={filter[0]}
+              key={filter[0]}
+              label={`${capitalizeFirstLetter(
+                filter[0]
+              )}: ${capitalizeFirstLetter(filter[1].toString())}`}
+              color="success"
+              onDelete={onDelete}
+            />
+          );
+        }
+      })}
+    </div>
+  );
 };
--- components/pages/home/components/filters-chips-panel/types.ts
+++ components/pages/home/components/filters-chips-panel/types.ts
@@ -1,6 +1,7 @@
-import { HTMLAttributes } from 'react';
+import { HTMLAttributes, MouseEventHandler } from 'react';
 import { PartialAdvertsFilters } from '../../types';
 
 export interface FiltersChipsPanelProps extends HTMLAttributes<HTMLDivElement> {
   filters: PartialAdvertsFilters;
+  onDelete?: MouseEventHandler<HTMLButtonElement>;
 }
