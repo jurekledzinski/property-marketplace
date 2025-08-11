9345d748a8305146616dd5b65ed33912e1406488
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-11 19:42:38 +0200

    Add onDelete from filter chips panel

--- components/pages/home/components/adverts-layout/AdvertsLayout.tsx
+++ components/pages/home/components/adverts-layout/AdvertsLayout.tsx
@@ -1,6 +1,7 @@
 'use clien';
 import styles from './AdvertsLayout.module.css';
 import { AdvertsLayoutProps } from './types';
+import { MouseEvent, useCallback } from 'react';
 import { useSetQuries } from '@/hooks';
 
 import {
@@ -12,6 +13,7 @@ import {
   CardsSection,
   useFilterForm,
   FiltersChipsPanel,
+  InputsAdvertsFilter,
 } from '@/components';
 
 export const AdvertsLayout = ({
@@ -20,13 +22,26 @@ export const AdvertsLayout = ({
   sortValue,
 }: AdvertsLayoutProps) => {
   const controlQueries = useSetQuries();
-  const { formControl, onSubmit, onResetAllFilters, reset } = useFilterForm({
-    ...controlQueries,
-    filters,
-  });
+  const { formControl, onSubmit, onResetFilter, onResetAllFilters, reset } =
+    useFilterForm({
+      ...controlQueries,
+      filters,
+    });
 
-  const empty = Object.values(formControl.watch()).some((item) => item.length);
-  const isFiltersEmpty = Object.values(filters).some((item) => item.length);
+  const empty = Object.values(formControl.watch()).some((item) => item?.length);
+  const isFiltersEmpty = Object.values(filters).some((item) => item?.length);
+
+  console.log('Watch', formControl.watch());
+
+  const onDelete = useCallback(
+    (e: MouseEvent<HTMLButtonElement>) => {
+      const target = e.currentTarget as HTMLButtonElement;
+      const name = target.name as keyof InputsAdvertsFilter;
+      const value = target.id;
+      onResetFilter(name, value);
+    },
+    [onResetFilter]
+  );
 
   return (
     <div className={styles.layout}>
@@ -35,7 +50,9 @@ export const AdvertsLayout = ({
           searchValue={decodeURIComponent(searchValue ?? '')}
           sortValue={decodeURIComponent(sortValue ?? '')}
         />
-        {isFiltersEmpty && <FiltersChipsPanel filters={filters} />}
+        {isFiltersEmpty && (
+          <FiltersChipsPanel filters={filters} onDelete={onDelete} />
+        )}
         <CardsSection />
       </div>
       <Backdrop open={false} />
