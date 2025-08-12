d78d6774d0c5cae4f3e061bd3b77565344538f0e
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-12 14:14:05 +0200

    Add icon buttons for theme filters and menu

--- components/shared/header/Header.tsx
+++ components/shared/header/Header.tsx
@@ -1,15 +1,54 @@
+'use client';
 import styles from './Header.module.css';
-import { AppBar, ButtonGroup, Container, Heading, Icon } from '@/components';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { useDrawer, useTheme } from '@/store';
+
+import {
+  AppBar,
+  ButtonGroup,
+  Container,
+  Heading,
+  IconButton,
+} from '@/components';
+import {
+  faBars,
+  faFilter,
+  faMoon,
+  faSun,
+} from '@fortawesome/free-solid-svg-icons';
 
 export const Header = () => {
+  const { mode, onChange } = useTheme();
+  const { onToggleFiltersPanel } = useDrawer();
+
   return (
     <AppBar className={styles.bar}>
       <Container m="m-center">
         <nav className={styles.nav}>
           <Heading level={4}>PlaceQuest</Heading>
           <ButtonGroup spacing="normal">
-            <Icon icon={faBars} />
+            {mode === 'light' ? (
+              <IconButton
+                icon={[faMoon]}
+                variant="minimal"
+                size="size-md"
+                onClick={() => onChange && onChange('dark')}
+              />
+            ) : (
+              <IconButton
+                icon={[faSun]}
+                variant="minimal"
+                size="size-md"
+                onClick={() => onChange && onChange('light')}
+              />
+            )}
+            <IconButton
+              className={styles.iconFilter}
+              icon={[faFilter]}
+              variant="minimal"
+              size="size-md"
+              onClick={() => onToggleFiltersPanel && onToggleFiltersPanel()}
+            />
+            <IconButton icon={[faBars]} variant="minimal" size="size-md" />
           </ButtonGroup>
         </nav>
       </Container>
