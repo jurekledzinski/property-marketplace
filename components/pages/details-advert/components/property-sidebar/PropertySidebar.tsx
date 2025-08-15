6210ca243d7479fa688289615ba2f7b77b781ba8
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-15 19:17:09 +0200

    Create property sidebar component

---
+++ components/pages/details-advert/components/property-sidebar/PropertySidebar.tsx
@@ -0,0 +1,11 @@
+import { PropertySidebarProps } from './types';
+import styles from './PropertySidebar.module.css';
+import { Box, Heading } from '@/components';
+
+export const PropertySidebar = ({}: PropertySidebarProps) => {
+  return (
+    <Box className={styles.sidebar}>
+      <Heading level={4}>Contact</Heading>
+    </Box>
+  );
+};
---
+++ components/pages/details-advert/components/property-sidebar/types.ts
@@ -0,0 +1,3 @@
+export type PropertySidebarProps = {
+  details?: null;
+};
