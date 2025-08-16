7a6b1870a644006d71c40000fcf4618a39f52bde
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-16 12:33:43 +0200

    Add dummy data to home page server side

--- app/(with-layout)/page.tsx
+++ app/(with-layout)/page.tsx
@@ -1,7 +1,80 @@
-import { HomePage, InputsAdvertsFilter } from '@/components';
+import { Home, InputsAdvertsFilter } from '@/components';
 import { HomePageProps } from './types';
 
-const Home = async ({ searchParams }: HomePageProps) => {
+const dataAdvertCards = [
+  {
+    area: 100,
+    bathrooms: 1,
+    city: 'Sittard',
+    country: 'Netherlands',
+    state: 'Limburg',
+    postalCode: '50-490',
+    price: 120000,
+    rooms: 3,
+    street: 'Akwarela 5',
+    title: 'Modern 3-Bedroom Home with Open-Concept Living',
+    status: 'Sale',
+    src: 'https://cdn.pixabay.com/photo/2025/06/10/11/21/view-9651981_1280.jpg',
+  },
+  {
+    area: 100,
+    bathrooms: 1,
+    city: 'Sittard',
+    country: 'Netherlands',
+    state: 'Limburg',
+    postalCode: '50-490',
+    price: 1200000,
+    rooms: 3,
+    street: 'Akwarela 5',
+    title: 'Contemporary Family Home with Garden & Garage',
+    status: 'Sale',
+    src: 'https://cdn.pixabay.com/photo/2017/05/28/18/48/ivy-2351778_1280.jpg',
+  },
+  {
+    area: 100,
+    bathrooms: 1,
+    city: 'Sittard',
+    country: 'Netherlands',
+    state: 'Limburg',
+    postalCode: '50-490',
+    price: 120000,
+    rooms: 3,
+    street: 'Akwarela 5',
+    title: 'Stylish Modern House for Sale in Prime Location',
+    status: 'Sale',
+    src: 'https://cdn.pixabay.com/photo/2017/05/31/10/23/manor-house-2359884_1280.jpg',
+  },
+  {
+    area: 100,
+    bathrooms: 1,
+    city: 'Sittard',
+    country: 'Netherlands',
+    state: 'Limburg',
+    postalCode: '50-490',
+    price: 120000,
+    rooms: 3,
+    street: 'Akwarela 5',
+    title: 'Architect-Designed Modern Home with Stunning Interiors',
+    status: 'Sale',
+    src: 'https://cdn.pixabay.com/photo/2017/06/21/16/33/farm-2427751_1280.jpg',
+  },
+  {
+    area: 100,
+    bathrooms: 1,
+    city: 'Sittard',
+    country: 'Netherlands',
+    state: 'Limburg',
+    postalCode: '50-490',
+    price: 120000,
+    rooms: 3,
+    street: 'Akwarela 5',
+    title: 'Tiny house',
+    status: 'Sale',
+    src: 'https://images.pexels.com/photos/3774243/pexels-photo-3774243.jpeg',
+  },
+];
+
+const HomePage = async ({ searchParams }: HomePageProps) => {
   const { search, sort, ...filters } = await searchParams;
 
   const decodedFilters = Object.fromEntries(
@@ -13,8 +86,13 @@ const Home = async ({ searchParams }: HomePageProps) => {
   ) as InputsAdvertsFilter;
 
   return (
-    <HomePage searchValue={search} sortValue={sort} filters={decodedFilters} />
+    <Home
+      advertCards={dataAdvertCards}
+      searchValue={search}
+      sortValue={sort}
+      filters={decodedFilters}
+    />
   );
 };
 
-export default Home;
+export default HomePage;
