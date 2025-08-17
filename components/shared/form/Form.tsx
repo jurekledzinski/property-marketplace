498197cca490b71be897371b6e49bc1519613c20
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-17 14:32:34 +0200

    Add to form component possibilty to add extra classname

--- components/shared/form/Form.tsx
+++ components/shared/form/Form.tsx
@@ -1,11 +1,12 @@
+import { classNames } from '@/helpers';
 import { FormProps } from './types';
 import { getClassForm } from './utils';
 
-export const Form = ({ children, ...props }: FormProps) => {
+export const Form = ({ className, children, ...props }: FormProps) => {
   const classes = getClassForm(props.orientation);
 
   return (
-    <form className={classes.form} {...props}>
+    <form className={classNames(classes.form, className)} {...props}>
       {children}
     </form>
   );
