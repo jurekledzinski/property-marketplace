9183e92382d363f5723c824ba8b293b531f2e37e
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-17 14:30:14 +0200

    Style property details advert

--- components/pages/details-advert/components/property-details/PropertyDetails.tsx
+++ components/pages/details-advert/components/property-details/PropertyDetails.tsx
@@ -1,6 +1,7 @@
-import { PropertyDetailsProps } from './types';
 import styles from './PropertyDetails.module.css';
-import { Box, Heading } from '@/components';
+import { amenitiesIcons } from './utils';
+import { Box, Heading, Icon, List, ListItem } from '@/components';
+import { PropertyDetailsProps } from './types';
 import { useFormatDetails } from './hooks';
 
 export const PropertyDetails = ({ details }: PropertyDetailsProps) => {
@@ -8,23 +9,39 @@ export const PropertyDetails = ({ details }: PropertyDetailsProps) => {
 
   return (
     <Box className={styles.details}>
-      <Heading level={4}>Property details</Heading>
-      <ul className={styles.list}>
+      <Heading level={4} mb="mb-sm">
+        Property details
+      </Heading>
+
+      <List>
         {Object.entries(formatedDetails).map((detail) => (
-          <li key={detail[0]} className={styles.item}>
+          <ListItem className={styles.detailItem} key={detail[0]}>
             <span>{detail[0]}</span>
             <span>{detail[1]}</span>
-          </li>
+          </ListItem>
         ))}
-      </ul>
+      </List>
+
+      <Heading level={4} mb="mb-sm" mt="mt-sm">
+        Description
+      </Heading>
 
       <p>{details.description}</p>
 
-      <Box>
-        <Heading level={4} mb="mb-sm">
-          Amenitis
-        </Heading>
-      </Box>
+      <Heading level={4} mb="mb-sm" mt="mt-sm">
+        Amenitis
+      </Heading>
+
+      <List className={styles.amenitiesList}>
+        {details.amenities.map((amenity) => (
+          <ListItem className={styles.amenityItem} key={amenity}>
+            <Icon
+              icon={amenitiesIcons[amenity as keyof typeof amenitiesIcons]}
+            />
+            {amenity}
+          </ListItem>
+        ))}
+      </List>
     </Box>
   );
 };
--- components/pages/details-advert/components/property-details/index.ts
+++ components/pages/details-advert/components/property-details/index.ts
@@ -1,2 +1,4 @@
+export * from './hooks';
 export * from './PropertyDetails';
 export * from './types';
+export * from './utils';
