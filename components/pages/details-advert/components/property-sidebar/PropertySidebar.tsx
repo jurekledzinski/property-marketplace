3b2d01ebb972af59379681fe8561f04338491d42
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-17 14:30:42 +0200

    Style property sidebar advert

--- components/pages/details-advert/components/property-sidebar/PropertySidebar.tsx
+++ components/pages/details-advert/components/property-sidebar/PropertySidebar.tsx
@@ -1,15 +1,38 @@
-import { PropertySidebarProps } from './types';
 import styles from './PropertySidebar.module.css';
-import { Box, Heading } from '@/components';
+import { Box, ContactForm, Heading, Icon, List, ListItem } from '@/components';
+import { faEnvelope, faPhone, faUser } from '@fortawesome/free-solid-svg-icons';
+import { PropertySidebarProps } from './types';
 
 export const PropertySidebar = ({
   advertiser,
   email,
   phone,
+  controls,
 }: PropertySidebarProps) => {
   return (
     <Box className={styles.sidebar}>
-      <Heading level={4}>Contact</Heading>
+      <Heading level={4} mb="mb-sm">
+        Contact
+      </Heading>
+      <List className={styles.userList}>
+        <ListItem className={styles.capitalized}>
+          <Icon icon={faUser} />
+          {advertiser}
+        </ListItem>
+        <ListItem className={styles.lowerCased}>
+          <Icon icon={faPhone} />
+          {phone}
+        </ListItem>
+        <ListItem className={styles.lowerCased}>
+          <Icon icon={faEnvelope} />
+          {email}
+        </ListItem>
+      </List>
+
+      <ContactForm
+        controls={controls.formControls}
+        onSubmit={controls.onSubmit}
+      />
     </Box>
   );
 };
--- components/pages/details-advert/components/property-sidebar/types.ts
+++ components/pages/details-advert/components/property-sidebar/types.ts
@@ -1,7 +1,9 @@
 import { Advert } from '@/models';
+import { useContactForm } from '@/components';
 
 export type PropertySidebarProps = {
   advertiser: Advert['advertiser'];
   phone: Advert['phone'];
   email: Advert['email'];
+  controls: ReturnType<typeof useContactForm>;
 };
