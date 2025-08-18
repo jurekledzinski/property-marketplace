4d4bcf48c2c92dd8cfd65d3331a48c8f05409650
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-18 19:25:45 +0200

    Create UserDashboard component

--- components/pages/user-dashboard/UserDashboard.tsx
+++ components/pages/user-dashboard/UserDashboard.tsx
@@ -1,11 +1,39 @@
-import { Container } from '@/components';
-import styles from './UserDashboard.module.css';
+'use client';
+import { getClassNamesUserDashboard } from './utils';
+import { useDrawer } from '@/store';
+
+import {
+  AdvertsStatusCard,
+  AdvertsTypeStatsCard,
+  Box,
+  Container,
+  Drawer,
+  DrawerPanel,
+  LatestAdvertsCard,
+  TopAdvertCard,
+  TotalAdvertsCard,
+  ViewAdvertsCard,
+} from '@/components';
 
 export const UserDashboard = () => {
+  const { openMenuPanel: open } = useDrawer();
+  const classes = getClassNamesUserDashboard({ open });
+
   return (
-    <div className={styles.dashboard}>
-      <Container m="m-center" maxWidth="mw-md">
-        UserDashboard component
+    <div className={classes.dashboard}>
+      <Container className={classes.container} m="m-center" maxWidth="mw-md">
+        <Drawer direction="left" open={open} variant="push">
+          <DrawerPanel />
+        </Drawer>
+
+        <Box className={classes.grid}>
+          <TotalAdvertsCard />
+          <AdvertsTypeStatsCard />
+          <AdvertsStatusCard />
+          <ViewAdvertsCard />
+          <TopAdvertCard />
+          <LatestAdvertsCard />
+        </Box>
       </Container>
     </div>
   );
