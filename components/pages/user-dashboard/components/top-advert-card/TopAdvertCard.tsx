dad0e933d6e97bd27a4a1e80fd3128ce93fe35b5
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-20 16:58:45 +0200

    Update top advert card

--- components/pages/user-dashboard/components/top-advert-card/TopAdvertCard.tsx
+++ components/pages/user-dashboard/components/top-advert-card/TopAdvertCard.tsx
@@ -1,5 +1,18 @@
 import styles from '../../UserDashboard.module.css';
+import { Card, CardContent, CardFooter, CardHeader, Icon } from '@/components';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 
 export const TopAdvertCard = () => {
-  return <div className={styles.topAdvert}>Top viewed advert card</div>;
+  return (
+    <Card className={styles.topAdvert}>
+      <CardContent className={styles.cardContent}>
+        <p style={{ fontSize: 20, fontWeight: 500 }}>14</p>
+        <p style={{ fontSize: 14 }}>Top viewed advert</p>
+      </CardContent>
+      <CardHeader className={styles.cardHeader}>
+        <Icon icon={faEye} size="2x" />
+      </CardHeader>
+      <CardFooter className={styles.cardFooter}></CardFooter>
+    </Card>
+  );
 };
--- components/pages/user-dashboard/components/total-adverts-card/TotalAdvertsCard.tsx
+++ components/pages/user-dashboard/components/total-adverts-card/TotalAdvertsCard.tsx
@@ -1,5 +1,18 @@
 import styles from '../../UserDashboard.module.css';
+import { Card, CardContent, CardFooter, CardHeader, Icon } from '@/components';
+import { faRectangleAd } from '@fortawesome/free-solid-svg-icons';
 
 export const TotalAdvertsCard = () => {
-  return <div className={styles.totalAdverts}>Total amount adverts</div>;
+  return (
+    <Card className={styles.totalAdverts}>
+      <CardContent className={styles.cardContent}>
+        <p style={{ fontSize: 20, fontWeight: 500 }}>14</p>
+        <p style={{ fontSize: 14 }}>Total adverts</p>
+      </CardContent>
+      <CardHeader className={styles.cardHeader}>
+        <Icon icon={faRectangleAd} size="2x" />
+      </CardHeader>
+      <CardFooter className={styles.cardFooter}></CardFooter>
+    </Card>
+  );
 };
