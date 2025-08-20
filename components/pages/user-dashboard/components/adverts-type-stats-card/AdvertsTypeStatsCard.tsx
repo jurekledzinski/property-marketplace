4adb1cd70c00572b6bcf6a1503d889bd14b8b7d6
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-20 16:57:07 +0200

    Update adverts type stats card

--- components/pages/user-dashboard/components/adverts-type-stats-card/AdvertsTypeStatsCard.tsx
+++ components/pages/user-dashboard/components/adverts-type-stats-card/AdvertsTypeStatsCard.tsx
@@ -1,5 +1,24 @@
 import styles from '../../UserDashboard.module.css';
+import { Card, CardContent, CardFooter, CardHeader, Icon } from '@/components';
+import { faHouseCircleCheck } from '@fortawesome/free-solid-svg-icons';
 
 export const AdvertsTypeStatsCard = () => {
-  return <div className={styles.typeAdverts}>Rend sale</div>;
+  return (
+    <Card className={styles.typeAdverts}>
+      <CardContent className={styles.cardContentHorizontal}>
+        <div>
+          <p style={{ fontSize: 20, fontWeight: 500 }}>1</p>
+          <p style={{ fontSize: 14 }}>Total rent</p>
+        </div>
+        <div>
+          <p style={{ fontSize: 20, fontWeight: 500 }}>2</p>
+          <p style={{ fontSize: 14 }}>Total sale</p>
+        </div>
+      </CardContent>
+      <CardHeader className={styles.cardHeader}>
+        <Icon icon={faHouseCircleCheck} size="2x" />
+      </CardHeader>
+      <CardFooter className={styles.cardFooter}></CardFooter>
+    </Card>
+  );
 };
