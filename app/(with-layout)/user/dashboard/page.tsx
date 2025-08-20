60ec4dac9949bd69b2b00becf7002d6f151ab73a
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-20 16:52:59 +0200

    Update dummy data to userdasboard

--- app/(with-layout)/user/dashboard/page.tsx
+++ app/(with-layout)/user/dashboard/page.tsx
@@ -1,7 +1,21 @@
 import { UserDashboard } from '@/components';
 
+const serverData = {
+  topAdvert: 'Advert 1',
+  total: { adverts: 12, messages: 3, rent: 3, sale: 1 },
+  views: [
+    { label: 'Modern house with 3 bedrooms', amount: 4 },
+    { label: 'Cousy traditional apartment in center old town', amount: 2 },
+    { label: 'Modern house in city', amount: 8 },
+  ],
+  stats: [
+    { label: 'Published', amount: 4 },
+    { label: 'InActive', amount: 8 },
+  ],
+};
+
 const UserDashboardPage = () => {
-  return <UserDashboard />;
+  return <UserDashboard dashboardAnalytics={serverData} />;
 };
 
 export default UserDashboardPage;
