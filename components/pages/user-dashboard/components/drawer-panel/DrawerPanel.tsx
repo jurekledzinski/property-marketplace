70412871e5fa09fa47d645b718b87c5fc11f0b5f
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-21 10:18:17 +0200

    Add links to dashboard menu

--- components/pages/user-dashboard/components/drawer-panel/DrawerPanel.tsx
+++ components/pages/user-dashboard/components/drawer-panel/DrawerPanel.tsx
@@ -1,5 +1,49 @@
+import Link from 'next/link';
 import styles from './DrawerPanel.module.css';
+import { Icon, List, ListItem } from '@/components';
+import {
+  faAd,
+  faAdd,
+  faEnvelope,
+  faGear,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 
 export const DrawerPanel = () => {
-  return <div className={styles.panel}>Drawer content</div>;
+  return (
+    <div className={styles.panel}>
+      <List className={styles.list}>
+        <ListItem>
+          <Link className={styles.listItem} href="/user/dashboard">
+            <Icon icon={faGear} />
+            Dashboard
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link className={styles.listItem} href="/user/adverts/new">
+            <Icon icon={faAdd} />
+            New Advert
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link className={styles.listItem} href="/user/adverts">
+            <Icon icon={faAd} />
+            My Adverts
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link className={styles.listItem} href="/user/profile">
+            <Icon icon={faUser} />
+            Profile
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link className={styles.listItem} href="/user/messages">
+            <Icon icon={faEnvelope} />
+            Messages
+          </Link>
+        </ListItem>
+      </List>
+    </div>
+  );
 };
