f10534e86159f1d063df9dab2081152d99e7ffe4
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-22 15:03:24 +0200

    Fix types for PropertyTypeSection

--- components/shared/property-form-sections/property-type-section/PropertyTypeSection.tsx
+++ components/shared/property-form-sections/property-type-section/PropertyTypeSection.tsx
@@ -1,16 +1,19 @@
 'use client';
 import { Controller } from 'react-hook-form';
+import { memo } from 'react';
 import { PropertyTypeSectionProps } from './types';
+
 import {
   Field,
+  Label,
   Message,
+  PropertyTypeFields,
   Select,
   SelectList,
   SelectOption,
   SelectPanel,
   SelectTrigger,
 } from '@/components';
-import { memo } from 'react';
 
 const conditons = [
   'New',
@@ -23,90 +26,94 @@ const conditons = [
   'Not Specified',
 ];
 
-export const PropertyTypeSection = memo(
-  ({
-    controls,
-    rulesStatus,
-    rulesCondition,
-    rulesType,
-  }: PropertyTypeSectionProps) => {
-    const { control, formState } = controls;
-    const { errors } = formState;
+const TypePart = <T extends PropertyTypeFields>({
+  controls,
+  errors,
+  labels = false,
+  nameCondition,
+  nameStatus,
+  nameType,
+  rulesStatus,
+  rulesCondition,
+  rulesType,
+}: PropertyTypeSectionProps<T>) => {
+  const { control } = controls;
 
-    return (
-      <>
-        <Field>
-          <Controller
-            name={'type'}
-            rules={rulesType}
-            control={control}
-            render={({ field: { onChange, ...rest } }) => (
-              <Select onChange={(id) => onChange(id)} {...rest}>
-                <SelectTrigger placeholder="Select type property" />
-                <SelectPanel>
-                  <SelectList>
-                    <SelectOption id="house">House</SelectOption>
-                    <SelectOption id="apartment">Apartment</SelectOption>
-                  </SelectList>
-                </SelectPanel>
-              </Select>
-            )}
-          />
-          {errors.type ? (
-            <Message variant="error">{errors.type.message}</Message>
-          ) : null}
-        </Field>
-        <Field>
-          <Controller
-            name={'condition'}
-            rules={rulesCondition}
-            control={control}
-            render={({ field: { onChange, ...rest } }) => (
-              <Select onChange={(id) => onChange(id)} {...rest}>
-                <SelectTrigger placeholder="Select technical condition" />
-                <SelectPanel>
-                  <SelectList>
-                    {conditons.map((condition) => (
-                      <SelectOption
-                        key={condition}
-                        id={condition.toLowerCase()}
-                      >
-                        {condition}
-                      </SelectOption>
-                    ))}
-                  </SelectList>
-                </SelectPanel>
-              </Select>
-            )}
-          />
-          {errors.condition ? (
-            <Message variant="error">{errors.condition.message}</Message>
-          ) : null}
-        </Field>
-        <Field>
-          <Controller
-            name={'status'}
-            rules={rulesStatus}
-            control={control}
-            render={({ field: { onChange, ...rest } }) => (
-              <Select onChange={(id) => onChange(id)} {...rest}>
-                <SelectTrigger placeholder="Select type advertisement" />
-                <SelectPanel>
-                  <SelectList>
-                    <SelectOption id="sale">Sale</SelectOption>
-                    <SelectOption id="rent">Rent</SelectOption>
-                  </SelectList>
-                </SelectPanel>
-              </Select>
-            )}
-          />
-          {errors.status ? (
-            <Message variant="error">{errors.status.message}</Message>
-          ) : null}
-        </Field>
-      </>
-    );
-  }
-);
+  return (
+    <>
+      <Field>
+        {labels && <Label>Type</Label>}
+        <Controller
+          name={nameType}
+          rules={rulesType}
+          control={control}
+          render={({ field: { onChange, ...rest } }) => (
+            <Select onChange={(id) => onChange(id)} {...rest}>
+              <SelectTrigger placeholder="Select type property" />
+              <SelectPanel>
+                <SelectList>
+                  <SelectOption id="house">House</SelectOption>
+                  <SelectOption id="apartment">Apartment</SelectOption>
+                </SelectList>
+              </SelectPanel>
+            </Select>
+          )}
+        />
+        {errors.type ? (
+          <Message variant="error">{errors.type.message}</Message>
+        ) : null}
+      </Field>
+      <Field>
+        {labels && <Label>Status</Label>}
+        <Controller
+          name={nameStatus}
+          rules={rulesStatus}
+          control={control}
+          render={({ field: { onChange, ...rest } }) => (
+            <Select onChange={(id) => onChange(id)} {...rest}>
+              <SelectTrigger placeholder="Select type advertisement" />
+              <SelectPanel>
+                <SelectList>
+                  <SelectOption id="sale">Sale</SelectOption>
+                  <SelectOption id="rent">Rent</SelectOption>
+                </SelectList>
+              </SelectPanel>
+            </Select>
+          )}
+        />
+        {errors.status ? (
+          <Message variant="error">{errors.status.message}</Message>
+        ) : null}
+      </Field>
+      <Field>
+        {labels && <Label>Condition</Label>}
+        <Controller
+          name={nameCondition}
+          rules={rulesCondition}
+          control={control}
+          render={({ field: { onChange, ...rest } }) => (
+            <Select onChange={(id) => onChange(id)} {...rest}>
+              <SelectTrigger placeholder="Select technical condition" />
+              <SelectPanel>
+                <SelectList>
+                  {conditons.map((condition) => (
+                    <SelectOption key={condition} id={condition.toLowerCase()}>
+                      {condition}
+                    </SelectOption>
+                  ))}
+                </SelectList>
+              </SelectPanel>
+            </Select>
+          )}
+        />
+        {errors.condition ? (
+          <Message variant="error">{errors.condition.message}</Message>
+        ) : null}
+      </Field>
+    </>
+  );
+};
 
-PropertyTypeSection.displayName = 'PropertyTypeSection';
+export const PropertyTypeSection = memo(TypePart) as typeof TypePart;
+
+TypePart.displayName = 'PropertyTypeSection';
--- components/shared/property-form-sections/property-type-section/types.ts
+++ components/shared/property-form-sections/property-type-section/types.ts
@@ -1,10 +1,19 @@
-import { InputsAdvertsFilter } from '@/components';
-import { Rule } from '../types';
-import { UseFormReturn } from 'react-hook-form';
+import { PropertyTypeFields } from '@/components';
+import {
+  FieldErrors,
+  Path,
+  UseControllerProps,
+  UseFormReturn,
+} from 'react-hook-form';
 
-export type PropertyTypeSectionProps = {
-  controls: UseFormReturn<InputsAdvertsFilter, unknown, InputsAdvertsFilter>;
-  rulesType?: Rule<'type'>;
-  rulesCondition?: Rule<'condition'>;
-  rulesStatus?: Rule<'status'>;
+export type PropertyTypeSectionProps<T extends PropertyTypeFields> = {
+  controls: UseFormReturn<T>;
+  errors: FieldErrors<PropertyTypeFields>;
+  nameType: Path<T>;
+  nameCondition: Path<T>;
+  nameStatus: Path<T>;
+  labels?: boolean;
+  rulesType?: UseControllerProps<T, Path<T>>['rules'];
+  rulesCondition?: UseControllerProps<T, Path<T>>['rules'];
+  rulesStatus?: UseControllerProps<T, Path<T>>['rules'];
 };
