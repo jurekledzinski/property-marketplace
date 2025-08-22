d86794fb43bc3b86dd1060fa04b7ec4a75758512
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-22 14:50:34 +0200

    Update filter form sections

--- components/pages/home/components/filter-panel/components/filter-form/FilterForm.tsx
+++ components/pages/home/components/filter-panel/components/filter-form/FilterForm.tsx
@@ -12,12 +12,41 @@ import {
 export const FilterForm = ({ controls, onSubmit, reset }: FilterFormProps) => {
   return (
     <Form id="filterForm" onSubmit={controls.handleSubmit(onSubmit)} noValidate>
-      <LocationSection key={reset.location} controls={controls} />
-      <PropertyTypeSection key={reset.type} controls={controls} />
-      <PropertyDetailsSection key={reset.details} controls={controls} />
+      <LocationSection
+        key={reset.location}
+        controls={controls}
+        errors={controls.formState.errors}
+        nameCity="city"
+        nameCountry="country"
+      />
+      <PropertyTypeSection
+        key={reset.type}
+        controls={controls}
+        errors={controls.formState.errors}
+        nameCondition="condition"
+        nameStatus="status"
+        nameType="type"
+      />
+      <PropertyDetailsSection
+        key={reset.details}
+        controls={controls}
+        errors={controls.formState.errors}
+        nameArea="area"
+        nameRooms="rooms"
+        nameYear="year"
+      />
       <PriceSection key={reset.price} controls={controls} />
-      <AmenitiesSection key={reset.amenities} controls={controls} />
-      <ArchitectureSection key={reset.architecture} controls={controls} />
+      <AmenitiesSection
+        key={reset.amenities}
+        controls={controls}
+        nameAmenities="amenities"
+      />
+      <ArchitectureSection
+        key={reset.architecture}
+        controls={controls}
+        errors={controls.formState.errors}
+        nameStyle="style"
+      />
     </Form>
   );
 };
