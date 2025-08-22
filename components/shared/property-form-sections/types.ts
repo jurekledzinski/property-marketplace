4f4eb05a3ed10ce87182eb4478591a9b53afe920
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-22 15:01:16 +0200

    Remove unnecessary types Rule form property form section

--- components/shared/property-form-sections/types.ts
+++
@@ -1,7 +0,0 @@
-import { InputsAdvertsFilter } from '@/components/pages';
-import { UseControllerProps } from 'react-hook-form';
-
-export type Rule<K extends keyof InputsAdvertsFilter> = UseControllerProps<
-  InputsAdvertsFilter,
-  K
->['rules'];
