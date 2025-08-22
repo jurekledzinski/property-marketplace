29715aa83c9f74e0989d383208966f05e982b480
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-22 15:02:25 +0200

    Fix types for LocationSection

--- components/shared/property-form-sections/location-section/LocationSection.tsx
+++ components/shared/property-form-sections/location-section/LocationSection.tsx
@@ -1,8 +1,12 @@
 'use client';
 import { Controller, useWatch } from 'react-hook-form';
 import { LocationSectionProps } from './types';
+import { memo } from 'react';
+
 import {
   Field,
+  Label,
+  LocationFields,
   Message,
   Select,
   SelectList,
@@ -10,73 +14,81 @@ import {
   SelectPanel,
   SelectTrigger,
 } from '@/components';
-import { memo } from 'react';
 
 const countries = ['Poland', 'Slovakia', 'Croatia'];
 const cities = ['Szczecin', 'Wroclaw', 'Krakow'];
 
-export const LocationSection = memo(
-  ({ controls, rulesCountry, rulesCity }: LocationSectionProps) => {
-    const { control, formState } = controls;
-    const { errors } = formState;
-    const country = useWatch({ name: 'country', control });
+export const LocationPart = <T extends LocationFields>({
+  controls,
+  rulesCountry,
+  rulesCity,
+  labels = false,
+  nameCountry,
+  nameCity,
+  errors,
+}: LocationSectionProps<T>) => {
+  const { control } = controls;
+  const country = useWatch({ control, name: nameCountry });
 
-    return (
-      <>
-        <Field>
-          <Controller
-            name={'country'}
-            control={control}
-            rules={rulesCountry}
-            render={({ field: { onChange, ...rest } }) => (
-              <Select onChange={(id) => onChange(id)} {...rest}>
-                <SelectTrigger placeholder="Select country" />
-                <SelectPanel>
-                  <SelectList>
-                    {countries.map((country) => (
-                      <SelectOption key={country} id={country.toLowerCase()}>
-                        {country}
-                      </SelectOption>
-                    ))}
-                  </SelectList>
-                </SelectPanel>
-              </Select>
-            )}
-          />
-          {errors.country ? (
-            <Message variant="error">{errors.country.message}</Message>
-          ) : null}
-        </Field>
-        <Field>
-          <Controller
-            name={'city'}
-            control={control}
-            rules={rulesCity}
-            render={({ field: { onChange, ...rest } }) => (
-              <Select onChange={(id) => onChange(id)} {...rest}>
-                <SelectTrigger
-                  placeholder="Select city"
-                  disabled={country === ''}
-                />
-                <SelectPanel>
-                  <SelectList>
-                    {cities.map((city) => (
-                      <SelectOption key={city} id={city.toLowerCase()}>
-                        {city}
-                      </SelectOption>
-                    ))}
-                  </SelectList>
-                </SelectPanel>
-              </Select>
-            )}
-          />
-          {errors.city ? (
-            <Message variant="error">{errors.city.message}</Message>
-          ) : null}
-        </Field>
-      </>
-    );
-  }
-);
+  return (
+    <>
+      <Field>
+        {labels && <Label>Country</Label>}
+        <Controller
+          name={nameCountry}
+          control={control}
+          rules={rulesCountry}
+          render={({ field: { onChange, ...rest } }) => (
+            <Select onChange={(id) => onChange(id)} {...rest}>
+              <SelectTrigger placeholder="Select country" />
+              <SelectPanel>
+                <SelectList>
+                  {countries.map((country) => (
+                    <SelectOption key={country} id={country.toLowerCase()}>
+                      {country}
+                    </SelectOption>
+                  ))}
+                </SelectList>
+              </SelectPanel>
+            </Select>
+          )}
+        />
+        {errors.country ? (
+          <Message variant="error">{errors.country.message}</Message>
+        ) : null}
+      </Field>
+      <Field>
+        {labels && <Label>City</Label>}
+        <Controller
+          name={nameCity}
+          control={control}
+          rules={rulesCity}
+          render={({ field: { onChange, ...rest } }) => (
+            <Select onChange={(id) => onChange(id)} {...rest}>
+              <SelectTrigger
+                placeholder="Select city"
+                disabled={country === ''}
+              />
+              <SelectPanel>
+                <SelectList>
+                  {cities.map((city) => (
+                    <SelectOption key={city} id={city.toLowerCase()}>
+                      {city}
+                    </SelectOption>
+                  ))}
+                </SelectList>
+              </SelectPanel>
+            </Select>
+          )}
+        />
+        {errors.city ? (
+          <Message variant="error">{errors.city.message}</Message>
+        ) : null}
+      </Field>
+    </>
+  );
+};
 
-LocationSection.displayName = 'LocationSection';
+export const LocationSection = memo(LocationPart) as typeof LocationPart;
+
+LocationPart.displayName = 'LocationSection';
--- components/shared/property-form-sections/location-section/types.ts
+++ components/shared/property-form-sections/location-section/types.ts
@@ -1,9 +1,17 @@
-import { InputsAdvertsFilter } from '@/components';
-import { Rule } from '../types';
-import { UseFormReturn } from 'react-hook-form';
+import { LocationFields } from '@/components';
+import {
+  FieldErrors,
+  Path,
+  UseControllerProps,
+  UseFormReturn,
+} from 'react-hook-form';
 
-export type LocationSectionProps = {
-  controls: UseFormReturn<InputsAdvertsFilter, unknown, InputsAdvertsFilter>;
-  rulesCountry?: Rule<'country'>;
-  rulesCity?: Rule<'city'>;
+export type LocationSectionProps<T extends LocationFields> = {
+  controls: UseFormReturn<T>;
+  errors: FieldErrors<LocationFields>;
+  nameCountry: Path<T>;
+  nameCity: Path<T>;
+  labels?: boolean;
+  rulesCountry?: UseControllerProps<T, Path<T>>['rules'];
+  rulesCity?: UseControllerProps<T, Path<T>>['rules'];
 };
