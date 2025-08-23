43d7c2118c3f9516a500582e346dba68a915ffd3
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-23 21:18:23 +0200

    Add dummy data on server page user adverts

--- app/(with-layout)/user/adverts/page.tsx
+++ app/(with-layout)/user/adverts/page.tsx
@@ -1,5 +1,19 @@
+import { AdvertsUser, UserAdverts } from '@/components';
+
+const stage = ['active', 'expired', 'expire soon'];
+
+const adverts = Array.from<AdvertsUser>({ length: 50 }).map((_, index) => ({
+  id: index.toString(),
+  userId: '123',
+  title: `Modern apartment in city center-${index}`,
+  type: `Apartment-${index}`,
+  stage: stage[Math.round(Math.random() * 2)],
+  createdAt: Date.now() - Math.floor(Math.random() * 10000000000),
+  actions: '',
+}));
+
 const UserAdvertsPage = () => {
-  return <div>User adverts page</div>;
+  return <UserAdverts adverts={adverts ?? []} />;
 };
 
 export default UserAdvertsPage;
