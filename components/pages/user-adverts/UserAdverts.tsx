10340062024cc94bc256fdf18b6903e83b98d094
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-24 09:49:24 +0200

    Add adverts pagintation component to userAdverts

--- components/pages/user-adverts/UserAdverts.tsx
+++ components/pages/user-adverts/UserAdverts.tsx
@@ -1,22 +1,13 @@
 'use client';
+import { Heading } from '@/components';
 import { UserAdvertsProps } from './types';
+
 import {
-  Heading,
-  Pagination,
-  PaginationArrow,
-  PaginationInfo,
-} from '@/components';
-import {
+  AdvertsPagination,
   AdvertsTable,
   useAdvertsColumns,
   useControlAdvertsTable,
 } from './components';
-import {
-  faAnglesLeft,
-  faAnglesRight,
-  faChevronLeft,
-  faChevronRight,
-} from '@fortawesome/free-solid-svg-icons';
 
 export const UserAdverts = ({ adverts = [] }: UserAdvertsProps) => {
   const columns = useAdvertsColumns();
@@ -43,45 +34,12 @@ export const UserAdverts = ({ adverts = [] }: UserAdvertsProps) => {
           const amountData = table.getPrePaginationRowModel().rows.length;
 
           return (
-            <Pagination
-              size="size-sm"
-              spacing="tight"
-              totalPages={table.getPageCount()}
-              variant="outlined"
-            >
-              <PaginationArrow
-                id="first"
-                label={faAnglesLeft}
-                onClick={() => table.setPageIndex(0)}
-                disabled={!table.getCanPreviousPage()}
-              />
-              <PaginationArrow
-                id="prev"
-                label={faChevronLeft}
-                onClick={() => table.previousPage()}
-                disabled={!table.getCanPreviousPage()}
-              />
-              <PaginationInfo
-                indexStart={currentPage * itemsPerPage + 1}
-                indexEnd={Math.min(
-                  (currentPage + 1) * itemsPerPage,
-                  amountData
-                )}
-                totalAmount={amountData}
-              />
-              <PaginationArrow
-                id="next"
-                label={faChevronRight}
-                onClick={() => table.nextPage()}
-                disabled={!table.getCanNextPage()}
-              />
-              <PaginationArrow
-                id="last"
-                label={faAnglesRight}
-                onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-                disabled={!table.getCanNextPage()}
-              />
-            </Pagination>
+            <AdvertsPagination
+              amountData={amountData}
+              currentPage={currentPage}
+              itemsPerPage={itemsPerPage}
+              table={table}
+            />
           );
         }}
       </AdvertsTable>
