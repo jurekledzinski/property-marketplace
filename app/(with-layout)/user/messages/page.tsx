ede9fe5955bc5973e95cf7f666aa983bbf20b98a
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-26 09:14:43 +0200

    Add dummy messages in user messages page

--- app/(with-layout)/user/messages/page.tsx
+++ app/(with-layout)/user/messages/page.tsx
@@ -1,5 +1,47 @@
+import { UserMessages } from '@/components';
+
+const dataMessages = [
+  {
+    id: '1',
+    advertiserId: '64f123abc456def789012345',
+    sender: {
+      name: 'Joe Doe 1',
+      email: 'joedoe@gmail.com',
+    },
+    title:
+      'Modern 2-Bedroom Apartment with Stunning City Views – Move-In Ready!',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit...',
+    createdAt: new Date().toISOString(),
+  },
+  {
+    id: '2',
+    advertiserId: '64f123abc456def789012345',
+    sender: {
+      name: 'Joe Doe 2',
+      email: 'joedoe@gmail.com',
+    },
+    title: 'Chic Urban Living: Spacious Apartment for Rent in Prime Location',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit...',
+    createdAt: new Date().toISOString(),
+  },
+  {
+    id: '3',
+    advertiserId: '64f123abc456def789012345',
+    sender: {
+      name: 'Joe Doe 3',
+      email: 'joedoe@gmail.com',
+    },
+    title:
+      'For Sale: Stylish Apartment with Balcony & Parking – Great Investment Opportunity',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit...',
+    createdAt: new Date().toISOString(),
+  },
+];
+
 const UserMessagesPage = () => {
-  return <div>User messages page</div>;
+  return <UserMessages messages={dataMessages} />;
 };
+
+<p></p>;
 
 export default UserMessagesPage;
