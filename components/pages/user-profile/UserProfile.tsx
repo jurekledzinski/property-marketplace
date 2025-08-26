be427b01b0b9ca4019c9261faa3a414c4f5a1614
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-26 09:38:16 +0200

    Add modal delete account to user profile

--- components/pages/user-profile/UserProfile.tsx
+++ components/pages/user-profile/UserProfile.tsx
@@ -1,22 +1,31 @@
 'use client';
 import styles from './UserProfile.module.css';
-import { Box, Button, ButtonGroup, Heading } from '@/components';
 import { PasswordForm, UserForm } from './components';
 import { usePasswordForm, useUserForm } from './hooks';
 import { usePasswordRules } from '../register';
 
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Heading,
+  Modal,
+  useControlModal,
+} from '@/components';
+
 export const UserProfile = () => {
-  const { formControl: userControl, onSubmit: onSubmitUser } = useUserForm();
-  const { formControl: passwordControl, onSubmit: onSubmitPassword } =
+  const { onClose, onOpen, isOpen } = useControlModal();
+  const { formControl: user, onSubmit: onSubmitUser } = useUserForm();
+  const { formControl: password, onSubmit: onSubmitPassword } =
     usePasswordForm();
   const passwordRules = usePasswordRules({
-    watch: passwordControl.watch,
+    watch: password.watch,
     nameConfirm: 'confirm',
     namePassword: 'password',
   });
 
-  console.log('USER WATCH', userControl.watch());
-  console.log('PASSWORD WATCH', passwordControl.watch());
+  console.log('USER WATCH', user.watch());
+  console.log('PASSWORD WATCH', password.watch());
 
   return (
     <>
@@ -24,9 +33,9 @@ export const UserProfile = () => {
         Profile
       </Heading>
       <Box className={styles.container}>
-        <UserForm controls={userControl} onSubmit={onSubmitUser} />
+        <UserForm controls={user} onSubmit={onSubmitUser} />
         <PasswordForm
-          controls={passwordControl}
+          controls={password}
           nameConfirm="confirm"
           namePassword="password"
           onSubmit={onSubmitPassword}
@@ -36,12 +45,25 @@ export const UserProfile = () => {
       <Heading level={4} mb="mb-md" mt="mt-md">
         Delete account
       </Heading>
+      <Modal
+        confirmText="Delete"
+        title="Delete account"
+        isOpen={isOpen}
+        onClose={onClose}
+        onCancel={onClose}
+        variant="negative"
+      >
+        Are you sure you want to delete your account? <br />
+        This action is permanent and cannot be undone. All your data will be
+        permanently removed.
+      </Modal>
       <ButtonGroup mb="mb-md">
         <Button
           color="negative"
           label="Delete account"
           size="size-md"
           variant="outlined"
+          onClick={onOpen}
         />
       </ButtonGroup>
     </>
