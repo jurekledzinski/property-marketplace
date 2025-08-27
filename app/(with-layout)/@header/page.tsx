22b84ba0cee8bc76a72c2e2a57f857857501d83c
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-27 20:11:45 +0200

    Pass session data from server component to header client component

--- app/(with-layout)/@header/page.tsx
+++ app/(with-layout)/@header/page.tsx
@@ -1,7 +1,10 @@
+import { auth } from '@/auth';
 import { Header as HeaderElement } from '@/components';
 
-const Header = () => {
-  return <HeaderElement />;
+const Header = async () => {
+  const session = await auth();
+
+  return <HeaderElement session={session?.user} />;
 };
 
 export default Header;
