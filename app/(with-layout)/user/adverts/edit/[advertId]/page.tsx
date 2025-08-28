c0e62d152b449528b8f48277e00b505401915f32
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-28 10:23:55 +0200

    Add dummy advert dummy data in edit server page

--- app/(with-layout)/user/adverts/edit/[advertId]/page.tsx
+++ app/(with-layout)/user/adverts/edit/[advertId]/page.tsx
@@ -1,5 +1,48 @@
-const EditAdvertPage = () => {
-  return <div>Edit advert page</div>;
+import { auth } from '@/auth';
+import { UserEditAdvert } from '@/components';
+import { EditAdvertPageProps } from './types';
+
+const editAdvert = {
+  advertiser: 'Joe Doe',
+  amenities: ['parking'],
+  area: '200',
+  bathrooms: '2',
+  city: 'Some city',
+  condition: 'new',
+  country: 'Some country',
+  description: 'Description',
+  email: 'joedoe@gmail.com',
+  images: [],
+  phone: '555444333',
+  postalCode: '4534HG',
+  price: '200',
+  rooms: '4',
+  status: 'sale',
+  street: 'Some street',
+  style: 'modern',
+  title: 'Modern house',
+  type: 'house',
+  userId: '123',
+  year: '2000',
+  dbImages: [
+    'https://cdn.pixabay.com/photo/2019/06/10/09/00/village-4263755_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2017/05/28/18/48/ivy-2351778_1280.jpg',
+    'https://cdn.pixabay.com/photo/2015/11/15/08/21/house-1044132_1280.jpg',
+    'https://cdn.pixabay.com/photo/2019/05/13/21/12/house-4200922_1280.jpg',
+  ],
+};
+
+// Tu trzeba params i pobierać będzie advert na podstawie advertId
+const EditAdvertPage = async ({ params }: EditAdvertPageProps) => {
+  const { advertId } = await params;
+
+  console.log('SERVER ADVERT EDIT PAGE ID ADVERT', advertId);
+
+  const session = await auth();
+
+  console.log('Edit advert server session', session);
+
+  return <UserEditAdvert advert={editAdvert} userId={session?.user.id ?? ''} />;
 };
 
 export default EditAdvertPage;
