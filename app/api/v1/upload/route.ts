f1e3463433df53f6ee80889c5be4891f89aa7420
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-29 22:52:01 +0200

    Create route api to upload images to imagekit

---
+++ app/api/v1/upload/route.ts
@@ -0,0 +1,84 @@
+import 'server-only';
+import busboy from 'busboy';
+import ImageKit from 'imagekit';
+import { errorResponseApi, successResponseApi } from '@/lib';
+import { NextRequest, NextResponse } from 'next/server';
+import { Readable } from 'node:stream';
+import { ReadableStream } from 'node:stream/web';
+import fs from 'node:fs';
+import path from 'node:path';
+import os from 'node:os';
+
+export const runtime = 'nodejs';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export const POST = async (request: NextRequest) => {
+  const imagekit = new ImageKit({
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY!,
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+    urlEndpoint: 'https://ik.imagekit.io/mdklwracd5rti',
+  });
+
+  return new Promise<NextResponse>((resolve, reject) => {
+    const headers = Object.fromEntries(request.headers);
+
+    const bb = busboy({ headers });
+    const uploadPromises: Promise<string>[] = [];
+
+    bb.on('file', (name, file, info) => {
+      const tempFilePath = path.join(
+        os.tmpdir(),
+        `${Date.now()}-${info.filename}`
+      );
+      const writeStream = fs.createWriteStream(tempFilePath);
+
+      file.pipe(writeStream);
+
+      const uploadPromise = new Promise<string>((res, rej) => {
+        writeStream.on('finish', async () => {
+          try {
+            const result = await imagekit.upload({
+              file: fs.createReadStream(tempFilePath),
+              fileName: info.filename,
+              folder: 'Houses',
+            });
+
+            fs.unlink(tempFilePath, () => {});
+            res(result.url);
+          } catch (err) {
+            rej(err);
+            return errorResponseApi({ status: 500, message: 'Upload failed' });
+          }
+        });
+      });
+
+      uploadPromises.push(uploadPromise);
+
+      file.on('error', () =>
+        errorResponseApi({ message: 'Upload failed', status: 500 })
+      );
+    });
+
+    bb.on('finish', async () => {
+      try {
+        const storedURLs = await Promise.all(uploadPromises);
+        resolve(successResponseApi({ payload: storedURLs }));
+      } catch {
+        reject(errorResponseApi({ message: 'Upload failed', status: 500 }));
+      }
+    });
+
+    bb.on('error', (err) => reject(err));
+
+    // Convert Fetch API stream → Node stream
+    const nodeStream = Readable.fromWeb(
+      request.body! as ReadableStream<Uint8Array<ArrayBuffer>>
+    );
+    nodeStream.pipe(bb);
+  });
+};
