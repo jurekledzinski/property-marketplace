be17854feb7b63fa237d1bd0d4b86f2f7503ca2c
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-29 23:07:02 +0200

    Updat advert model schema

--- models/advert.ts
+++ models/advert.ts
@@ -12,20 +12,24 @@ export const AdvertSchema = z.object({
   postalCode: z.string(),
   title: z.string(),
   description: z.string(),
+  files: z.array(z.instanceof(File)).optional(),
   images: z.array(z.string()),
-  dbImages: z.array(z.string()).optional(),
   deleteImagesIds: z.array(z.string()).optional(),
   type: z.string(), // apartment | house
   status: z.string(), // rent | sale
   condition: z.string(),
-  price: z.number(),
-  year: z.number(),
-  area: z.number(),
-  rooms: z.number(),
-  bathrooms: z.number(),
+  price: z.coerce.number(),
+  year: z.coerce.number(),
+  area: z.coerce.number(),
+  rooms: z.coerce.number(),
+  bathrooms: z.coerce.number(),
 
   style: z.string(),
   amenities: z.array(z.string()),
+  createdAt: z.string().optional(),
 });
 
 export type Advert = z.infer<typeof AdvertSchema>;
+
+//images = files
+//dbImages = images
