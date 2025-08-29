f25fb7ddb68676731e890352266d8adf2816b4cc
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-29 22:58:05 +0200

    Connect newAdvert actoon with advert form

--- components/pages/user-new-advert/UserNewAdvert.tsx
+++ components/pages/user-new-advert/UserNewAdvert.tsx
@@ -1,17 +1,33 @@
 'use client';
 import { AdvertForm } from './components';
 import { Heading, UserNewAdvertProps } from '@/components';
+import { initialState } from '@/constants';
+import { newAdvert } from '@/actions';
+import { useActionState } from 'react';
 import { useAdvertForm } from './hooks';
 
 export const UserNewAdvert = ({ userId }: UserNewAdvertProps) => {
-  const form = useAdvertForm({ userId });
+  const [state, action, isPending] = useActionState(newAdvert, initialState);
+
+  const form = useAdvertForm({
+    userId,
+    isPending,
+    isSuccess: state.success,
+    onSubmitForm: action,
+  });
+
+  console.log('state', state);
 
   return (
     <>
       <Heading level={4} mb="mb-md" mt="mt-sm">
         Add New Advert
       </Heading>
-      <AdvertForm controls={form.formControl} onSubmit={form.onSubmit} />
+      <AdvertForm
+        controls={form.formControl}
+        onSubmit={form.onSubmit}
+        reset={form.reset}
+      />
     </>
   );
 };
