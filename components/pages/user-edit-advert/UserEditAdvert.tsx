9716545947bc3da188933e02a9c3da6f1d6c6d1a
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-29 22:57:28 +0200

    Add props to useAdvertForm for useActionState

--- components/pages/user-edit-advert/UserEditAdvert.tsx
+++ components/pages/user-edit-advert/UserEditAdvert.tsx
@@ -4,14 +4,19 @@ import { Heading } from '@/components';
 import { UserEditAdvertProps } from './types';
 
 export const UserEditAdvert = ({ advert, userId }: UserEditAdvertProps) => {
-  const form = useAdvertForm({ advert, userId });
-
+  const { formControl, onSubmit, reset } = useAdvertForm({
+    advert,
+    userId,
+    isPending: false,
+    isSuccess: false,
+    onSubmitForm: () => {},
+  });
   return (
     <>
       <Heading level={4} mb="mb-md" mt="mt-sm">
         Edit Advert
       </Heading>
-      <AdvertForm controls={form.formControl} onSubmit={form.onSubmit} />
+      <AdvertForm controls={formControl} onSubmit={onSubmit} reset={reset} />
     </>
   );
 };
