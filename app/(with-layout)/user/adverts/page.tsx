119de7df135b49a2d7d75a7b8c577284c63f94ea
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-29 22:49:03 +0200

    Add fetch adverts in server adverts page

--- app/(with-layout)/user/adverts/page.tsx
+++ app/(with-layout)/user/adverts/page.tsx
@@ -1,19 +1,22 @@
 import { AdvertsUser, UserAdverts } from '@/components';
+import { getAdverts } from '@/lib';
 
-const stage = ['active', 'expired', 'expire soon'];
+// const stage = ['active', 'expired', 'expire soon'];
 
-const adverts = Array.from<AdvertsUser>({ length: 50 }).map((_, index) => ({
-  id: index.toString(),
-  userId: '123',
-  title: `Modern apartment in city center-${index}`,
-  type: `Apartment-${index}`,
-  stage: stage[Math.round(Math.random() * 2)],
-  createdAt: Date.now() - Math.floor(Math.random() * 10000000000),
-  actions: '',
-}));
+// const adverts = Array.from<AdvertsUser>({ length: 50 }).map((_, index) => ({
+//   id: index.toString(),
+//   userId: '123',
+//   title: `Modern apartment in city center-${index}`,
+//   type: `Apartment-${index}`,
+//   stage: stage[Math.round(Math.random() * 2)],
+//   createdAt: Date.now() - Math.floor(Math.random() * 10000000000),
+//   actions: '',
+// }));
 
-const UserAdvertsPage = () => {
-  return <UserAdverts adverts={adverts ?? []} />;
+const UserAdvertsPage = async () => {
+  const data = await getAdverts();
+
+  return <UserAdverts adverts={data ? data : []} />;
 };
 
 export default UserAdvertsPage;
