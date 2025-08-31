79b4af1c2f846bdf3f8fd6dc5f7a03af8119f3b4
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-08-31 02:22:28 +0200

    Add more api endpoints

--- lib/api/configApi.ts
+++ lib/api/configApi.ts
@@ -1,7 +1,9 @@
 import 'server-only';
 
 export const endpoints = {
-  adverts: (domain: string) => `${domain}/api/v1/adverts`,
+  userAdverts: (domain: string) => `${domain}/api/v1/user-adverts`,
+  userAdvert: (domain: string, advertId?: string) =>
+    `${domain}/api/v1/user-advert?id=${advertId}`,
   user: (domain: string, userId: string) =>
     `${domain}/api/v1/user?id=${userId}`,
 };
