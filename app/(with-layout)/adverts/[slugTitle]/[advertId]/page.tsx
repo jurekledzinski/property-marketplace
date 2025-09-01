b4daca300a509c0bd3530241910b028c52c4d583
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-02 01:35:36 +0200

    Update dummy data images in details advert

--- app/(with-layout)/adverts/[slugTitle]/[advertId]/page.tsx
+++ app/(with-layout)/adverts/[slugTitle]/[advertId]/page.tsx
@@ -1,25 +1,30 @@
 import { DetailsAdvert } from '@/components';
 
-const images: { url: string; fileId: string }[] = [
+const images: { url: string; fileId: string; name: string }[] = [
   {
     url: 'https://cdn.pixabay.com/photo/2019/06/10/09/00/village-4263755_1280.jpg',
     fileId: '1',
+    name: 'House 1',
   },
   {
     url: 'https://cdn.pixabay.com/photo/2017/07/07/13/55/giethoorn-2481483_1280.jpg',
     fileId: '1',
+    name: 'House 2',
   },
   {
     url: 'https://cdn.pixabay.com/photo/2013/10/01/15/01/holiday-home-189181_1280.jpg',
     fileId: '1',
+    name: 'House 3',
   },
   {
     url: 'https://cdn.pixabay.com/photo/2017/06/21/16/33/farm-2427751_1280.jpg',
     fileId: '1',
+    name: 'House 4',
   },
   {
     url: 'https://cdn.pixabay.com/photo/2017/05/28/18/48/ivy-2351778_960_720.jpg',
     fileId: '1',
+    name: 'House 5',
   },
 ];
 
