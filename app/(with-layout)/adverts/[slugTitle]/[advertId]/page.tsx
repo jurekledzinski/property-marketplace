ea63ba74311e0b71b0c30fd13180e94a2643dcd2
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-01 19:27:10 +0200

    Edit dummy data in advert details

--- app/(with-layout)/adverts/[slugTitle]/[advertId]/page.tsx
+++ app/(with-layout)/adverts/[slugTitle]/[advertId]/page.tsx
@@ -1,11 +1,26 @@
 import { DetailsAdvert } from '@/components';
 
-const images: string[] = [
-  'https://cdn.pixabay.com/photo/2019/06/10/09/00/village-4263755_1280.jpg',
-  'https://cdn.pixabay.com/photo/2017/07/07/13/55/giethoorn-2481483_1280.jpg',
-  'https://cdn.pixabay.com/photo/2013/10/01/15/01/holiday-home-189181_1280.jpg',
-  'https://cdn.pixabay.com/photo/2017/06/21/16/33/farm-2427751_1280.jpg',
-  'https://cdn.pixabay.com/photo/2017/05/28/18/48/ivy-2351778_960_720.jpg',
+const images: { url: string; fileId: string }[] = [
+  {
+    url: 'https://cdn.pixabay.com/photo/2019/06/10/09/00/village-4263755_1280.jpg',
+    fileId: '1',
+  },
+  {
+    url: 'https://cdn.pixabay.com/photo/2017/07/07/13/55/giethoorn-2481483_1280.jpg',
+    fileId: '1',
+  },
+  {
+    url: 'https://cdn.pixabay.com/photo/2013/10/01/15/01/holiday-home-189181_1280.jpg',
+    fileId: '1',
+  },
+  {
+    url: 'https://cdn.pixabay.com/photo/2017/06/21/16/33/farm-2427751_1280.jpg',
+    fileId: '1',
+  },
+  {
+    url: 'https://cdn.pixabay.com/photo/2017/05/28/18/48/ivy-2351778_960_720.jpg',
+    fileId: '1',
+  },
 ];
 
 const amenities = [
