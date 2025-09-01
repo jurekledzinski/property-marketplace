0da78483b78284c032d3dca7479394ff5622689f
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-02 01:33:31 +0200

    Work on remove uploaded files and local remove files in form new adver

--- components/pages/user-new-advert/components/images-section/hooks/remove-preview-files/useRemovePreviewFiles.tsx
+++ components/pages/user-new-advert/components/images-section/hooks/remove-preview-files/useRemovePreviewFiles.tsx
@@ -3,30 +3,41 @@ import { UseRemovePreviewFilesProps } from './types';
 const filterImages = (value: string, item: string) => value !== item;
 
 export const useRemovePreviewFiles = ({
+  removeUploadedFiles,
   setValue,
   watch,
 }: UseRemovePreviewFilesProps) => {
   const onRemove = (
     index: number,
-    file: { url: string; fileId: string } | File
+    file: { fileId: string; name: string; url: string } | File
   ) => {
     const deleteImages = watch('deleteImagesIds')!;
     const images = watch('images')!;
     const files = watch('files') ?? [];
 
+    // Remove dopiero po usunięciu z imagekit ale w funkcji remove files imagekit
+    //
+
     if (file && file instanceof File) {
-      const filtered = files.filter((i) => filterImages(i.name, file.name));
-
-      setValue('files', filtered, {
-        shouldValidate: true,
-        shouldDirty: false,
-      });
+      // Tego nie potrzeba już chyba ponieważ ja wyświetlam z images tylko
+      //   po upload do imagekit a nie z files
+      // A gdy dodaje zdjecia to po upload sukces usuwam z files i gdy rejected także
+      //   const filtered = files.filter((i) => filterImages(i.name, file.name));
+      //   setValue('files', filtered, {
+      //     shouldValidate: true,
+      //     shouldDirty: false,
+      //   });
     } else {
-      setValue('deleteImagesIds', [...deleteImages, file.fileId]);
+      const deletedIds = [
+        ...deleteImages,
+        { fileId: file.fileId, name: file.name },
+      ];
+      removeUploadedFiles(deletedIds);
+      //   setValue('deleteImagesIds', deletedIds);
 
-      const filtered = images.filter((i) => filterImages(i.url, file.url));
+      //   const filtered = images.filter((i) => filterImages(i.url, file.url));
 
-      setValue('images', filtered);
+      //   setValue('images', filtered);
     }
   };
 
