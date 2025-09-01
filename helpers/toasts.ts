53a785df5714bf865775e0914cb6951c9af0be2d
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-01 19:37:08 +0200

    Create showPromisToast helper function

--- helpers/toasts.ts
+++ helpers/toasts.ts
@@ -7,3 +7,26 @@ export const showSuccessToast = (message: string, autoClose?: number) => {
 export const showErrorToast = (message: string, autoClose?: number) => {
   toast.error(message, { ...(autoClose && { duration: autoClose }) });
 };
+
+const creatMessage = (message?: string) => (message ?? '').slice(0, 20);
+const toastMessage = (message: string) => `${message}`;
+
+export const showPromisToast = <T extends Response>({
+  autoClose,
+  message,
+  promise,
+}: {
+  promise: Promise<T>;
+  message?: string;
+  autoClose?: number;
+}) => {
+  toast.promise(
+    promise,
+    {
+      loading: 'Saving...',
+      success: toastMessage(`Image saved ${creatMessage(message)}`),
+      error: toastMessage(`Could not save ${creatMessage(message)}`),
+    },
+    { ...(autoClose && { duration: autoClose }) }
+  );
+};
