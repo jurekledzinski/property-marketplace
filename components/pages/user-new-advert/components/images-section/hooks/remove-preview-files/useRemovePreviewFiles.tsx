14d8b05a1eab332621aa3fd107da5a921a18513f
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-01 19:32:46 +0200

    Change access to images files in useRemovePreviewFiles

--- components/pages/user-new-advert/components/images-section/hooks/remove-preview-files/useRemovePreviewFiles.tsx
+++ components/pages/user-new-advert/components/images-section/hooks/remove-preview-files/useRemovePreviewFiles.tsx
@@ -6,24 +6,27 @@ export const useRemovePreviewFiles = ({
   setValue,
   watch,
 }: UseRemovePreviewFilesProps) => {
-  const onRemove = (index: number, file: string | File) => {
+  const onRemove = (
+    index: number,
+    file: { url: string; fileId: string } | File
+  ) => {
     const deleteImages = watch('deleteImagesIds')!;
     const images = watch('images')!;
     const files = watch('files') ?? [];
 
-    if (typeof file === 'string') {
-      setValue('deleteImagesIds', [...deleteImages, file]);
-
-      const filtered = images.filter((i) => filterImages(i, file));
-
-      setValue('images', filtered);
-    } else {
+    if (file && file instanceof File) {
       const filtered = files.filter((i) => filterImages(i.name, file.name));
 
       setValue('files', filtered, {
         shouldValidate: true,
         shouldDirty: false,
       });
+    } else {
+      setValue('deleteImagesIds', [...deleteImages, file.fileId]);
+
+      const filtered = images.filter((i) => filterImages(i.url, file.url));
+
+      setValue('images', filtered);
     }
   };
 
