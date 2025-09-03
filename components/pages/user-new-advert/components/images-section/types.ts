c4e2429759d08029082e375e448b3e7b3f6c8ed8
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-03 15:37:51 +0200

    Change passed props names and  types in image section component

--- components/pages/user-new-advert/components/images-section/types.ts
+++ components/pages/user-new-advert/components/images-section/types.ts
@@ -1,8 +1,10 @@
+import { OnError } from '@/components';
 import { InputsAvert } from '../../hooks';
 import { UseFormReturn } from 'react-hook-form';
 
 export type ImagesSectionProps = {
   controls: UseFormReturn<InputsAvert, unknown, InputsAvert>;
-  removeUploadedFiles: (deletedIds: { fileId: string; name: string }[]) => void;
+  deleteUploadedFiles: (deletedIds: { fileId: string; name: string }[]) => void;
   uploadFiles: (files: File[]) => Promise<void>;
+  validationInfo: OnError;
 };
