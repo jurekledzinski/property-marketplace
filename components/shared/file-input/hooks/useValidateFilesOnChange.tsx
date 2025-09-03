41b8be95df39dfa74a6daf251e7bb37231c33941
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-03 15:29:31 +0200

    Refactor useValidateFilesOnChange hook add return invalid files results

--- components/shared/file-input/hooks/useValidateFilesOnChange.tsx
+++ components/shared/file-input/hooks/useValidateFilesOnChange.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, DragEvent } from 'react';
 import { FileMimeType } from '@/types';
 import { formatFileSize } from '@/helpers';
 import { UseValidateFilesOnChangeProps } from './types';
@@ -9,35 +10,50 @@ export const useValidateFilesOnChange = ({
   onError,
 }: UseValidateFilesOnChangeProps) => {
   const checkFileType = (files: File[]) => {
+    const invalidFiles: string[] = [];
+
     files.forEach((file) => {
       if (!allowTypes.includes(file.type as FileMimeType) && onError) {
-        onError('type', file.type, file.name);
+        onError({ type: 'type', details: file.type, name: file.name });
+        invalidFiles.push(file.name);
       }
     });
+
+    return invalidFiles;
   };
 
   const checkMaxSize = (files: File[]) => {
     if (!maxSize || maxSize.length < 2) return;
+    const invalidFiles: string[] = [];
 
     files.forEach((file) => {
       const formattedSize = formatFileSize(file.size, maxSize[1]);
       if (parseFloat(formattedSize) > maxSize[0] && onError) {
-        onError('size', formattedSize, file.name);
+        onError({ type: 'size', details: formattedSize, name: file.name });
+        invalidFiles.push(file.name);
       }
     });
+
+    return invalidFiles;
   };
 
   const checkMaxAmountFiles = (files: File[]) => {
     if (!maxAmount || !onError) return;
-    if (files.length > maxAmount) onError('amount', `${files.length}`);
+    if (files.length > maxAmount)
+      onError({ details: files.length.toString(), type: 'amount' });
   };
 
-  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files) return;
+  const onChangeInput = (
+    e: ChangeEvent<HTMLInputElement> | DragEvent<HTMLDivElement>
+  ) => {
+    const files = 'dataTransfer' in e ? e.dataTransfer.files : e.target.files;
+    if (!files) return [];
 
-    checkFileType([...e.target.files]);
-    checkMaxSize([...e.target.files]);
-    checkMaxAmountFiles([...e.target.files]);
+    const invalidTypes = checkFileType([...files]);
+    const invalidMaxSizes = checkMaxSize([...files]);
+    checkMaxAmountFiles([...files]);
+
+    return invalidTypes.concat(invalidMaxSizes ?? []);
   };
 
   return onChangeInput;
