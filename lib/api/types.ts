d54971087eb08e68dbf97c30d2e466a67daac4b5
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-08 22:57:57 +0200

    Update types for api

--- lib/api/types.ts
+++ lib/api/types.ts
@@ -1,8 +1,28 @@
 import { Advert } from '@/models';
+import { ReadonlyHeaders } from 'next/dist/server/web/spec-extension/adapters/headers';
 
 export interface UserAdvertsTable
-  extends Pick<Advert, 'userId' | 'title' | 'type' | 'createdAt'> {
+  extends Pick<Advert, 'userId' | 'title' | 'type'> {
   id: string;
   stage?: string;
   actions?: string;
+  createdAt?: string;
+}
+
+export type ApiSuccessResponse<T> = {
+  message?: string;
+  payload?: T;
+  success: true;
+};
+
+export type ApiErrorResponse = {
+  message?: string;
+  status?: number;
+  success: false;
+};
+
+export interface FetchAPIProps extends RequestInit, NextFetchRequestConfig {
+  url: string;
+  headers?: ReadonlyHeaders;
+  method?: 'DELETE' | 'GET' | 'POST' | 'PUT';
 }
