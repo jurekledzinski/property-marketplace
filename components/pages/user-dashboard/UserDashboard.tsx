3cb905c2e0ecb143812d5fb06a99359af0a47db9
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-08 22:42:38 +0200

    Add query client provider to user dashboard

--- components/pages/user-dashboard/UserDashboard.tsx
+++ components/pages/user-dashboard/UserDashboard.tsx
@@ -1,21 +1,26 @@
 'use client';
 import { Box, Container, Drawer, DrawerPanel } from '@/components';
 import { getClassNamesUserDashboard } from './utils';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useDrawer } from '@/store';
 import { UserDashboardProps } from './types';
+
+export const queryClient = new QueryClient();
 
 export const UserDashboard = ({ children }: UserDashboardProps) => {
   const { openMenuPanel: open } = useDrawer();
   const classes = getClassNamesUserDashboard({ open });
 
   return (
-    <Box className={classes.dashboard}>
-      <Container className={classes.container} m="m-center" maxWidth="mw-md">
-        <Drawer direction="left" top={54} open={open} variant="push">
-          <DrawerPanel />
-        </Drawer>
-        <Box className={classes.content}>{children}</Box>
-      </Container>
-    </Box>
+    <QueryClientProvider client={queryClient}>
+      <Box className={classes.dashboard}>
+        <Container className={classes.container} m="m-center" maxWidth="mw-md">
+          <Drawer direction="left" top={54} open={open} variant="push">
+            <DrawerPanel />
+          </Drawer>
+          <Box className={classes.content}>{children}</Box>
+        </Container>
+      </Box>
+    </QueryClientProvider>
   );
 };
