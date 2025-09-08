d6f8044d2cdbb31af8a8009625aa5ebc94f40e83
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-08 22:44:58 +0200

    Update types delete upload files callback

--- components/pages/user-new-advert/components/advert-form/types.ts
+++ components/pages/user-new-advert/components/advert-form/types.ts
@@ -7,7 +7,12 @@ export type AdvertFormProps = {
   onSubmit: SubmitHandler<InputsAvert>;
   uploadFiles: (files: File[]) => Promise<void>;
   reset: Record<string, string>;
-  deleteUploadedFiles: (deletedIds: { fileId: string; name: string }[]) => void;
+  deleteUploadedFiles: (
+    index: number,
+    file:
+      | { fileId: string; name: string; url: string; isOriginal?: boolean }
+      | File
+  ) => void;
   validationInfo: OnError;
   isPending?: boolean;
 };
