790f9e223cb9fbdad77514ecd82ad76691e3f615
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-08 22:43:45 +0200

    Update user edit advert component

--- components/pages/user-edit-advert/UserEditAdvert.tsx
+++ components/pages/user-edit-advert/UserEditAdvert.tsx
@@ -1,22 +1,49 @@
 'use client';
-import { AdvertForm, useAdvertForm } from '@/components';
-import { Heading } from '@/components';
+import { editAdvert } from '@/actions';
+import { initialState } from '@/constants';
+import { showSuccessToast } from '@/helpers';
+import { useActionState } from 'react';
 import { UserEditAdvertProps } from './types';
 
+import {
+  Heading,
+  AdvertForm,
+  useAdvertFormWithUploads,
+  validationFilesInfo,
+} from '@/components';
+import { useRouter, usePathname } from 'next/navigation';
+
 export const UserEditAdvert = ({ advert, userId }: UserEditAdvertProps) => {
-  const { formControl, onSubmit, reset } = useAdvertForm({
+  const router = useRouter();
+  const [state, action, isPending] = useActionState(editAdvert, initialState);
+
+  const { deleteUploadedFiles, form, uploadFiles } = useAdvertFormWithUploads({
+    action,
     advert,
+    isPending,
+    mode: 'edit',
+    success: state.success,
     userId,
-    isPending: false,
-    isSuccess: false,
-    onSubmitForm: () => {},
+    onSuccess: () => {
+      showSuccessToast(state.message);
+      router.push('/user/adverts');
+    },
   });
+
   return (
     <>
       <Heading level={4} mb="mb-md" mt="mt-sm">
         Edit Advert
       </Heading>
-      <AdvertForm controls={formControl} onSubmit={onSubmit} reset={reset} />
+      <AdvertForm
+        controls={form.formControl}
+        onSubmit={form.onSubmit}
+        reset={form.reset}
+        deleteUploadedFiles={deleteUploadedFiles}
+        uploadFiles={uploadFiles}
+        validationInfo={validationFilesInfo}
+        isPending={false}
+      />
     </>
   );
 };
