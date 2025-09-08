5a5351aff71802a36bf9aa3f9695bcc09e000677
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-08 22:49:49 +0200

    Update types for preview files

--- components/shared/preview-files/components/card-file-preview/types.ts
+++ components/shared/preview-files/components/card-file-preview/types.ts
@@ -1,9 +1,13 @@
 export type CardFilePreviewProps = {
-  file: File | { fileId: string; name: string; url: string };
+  file:
+    | File
+    | { fileId: string; name: string; url: string; isOriginal?: boolean };
   index: number;
   onRemove: (
     index: number,
-    file: { fileId: string; name: string; url: string } | File
+    file:
+      | { fileId: string; name: string; url: string; isOriginal?: boolean }
+      | File
   ) => void;
   gridPlacement?: 'column' | 'grid' | 'row';
 };
--- components/shared/preview-files/types.ts
+++ components/shared/preview-files/types.ts
@@ -1,9 +1,14 @@
 export type PreviewFileProps = {
-  children?: React.ReactNode;
-  images: (File | { fileId: string; name: string; url: string })[];
-  gridPlacement?: 'column' | 'grid' | 'row';
+  images: (
+    | File
+    | { fileId: string; name: string; url: string; isOriginal?: boolean }
+  )[];
   onRemove: (
     index: number,
-    file: { fileId: string; name: string; url: string } | File
+    file:
+      | { fileId: string; name: string; url: string; isOriginal?: boolean }
+      | File
   ) => void;
+  children?: React.ReactNode;
+  gridPlacement?: 'column' | 'grid' | 'row';
 };
