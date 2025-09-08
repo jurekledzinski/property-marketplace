0ec612d1a41269b5d677593daaa025ba46144542
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-08 22:53:34 +0200

    Create pendingRequest helper function

---
+++ hooks/control-upload-files/utils/pendingRequests.ts
@@ -0,0 +1,24 @@
+import { fetchFiles, fetchFilesFailed, fetchFilesResponse } from './fetchFiles';
+import { showPromisToast } from '@/helpers';
+import { toastDeleteOptions, toastUploadOptions } from './toastPromiseOptions';
+import { tryCatchFiles } from './tryCatch';
+
+export const pendingRequests = (
+  method: 'DELETE' | 'POST',
+  url: string,
+  formData: FormData,
+  fileName: string
+) => {
+  return tryCatchFiles({
+    promise: () => fetchFiles(url, method, formData),
+    onError: (e) => fetchFilesFailed(e),
+    onSuccess: (res) => fetchFilesResponse(res),
+    toastPromise: (res) => {
+      if (method === 'DELETE') {
+        showPromisToast(toastDeleteOptions(res, fileName));
+      } else {
+        showPromisToast(toastUploadOptions(res, fileName));
+      }
+    },
+  });
+};
