e3b862ccc69b57791bd3a4694222e342ea933c53
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-11 12:54:44 +0200

    Update api error class

--- helpers/error.ts
+++ helpers/error.ts
@@ -1,10 +1,22 @@
 export class ApiError<T extends object> extends Error {
   payload?: T;
   success?: boolean;
-  constructor(message?: string, payload?: T, success?: boolean) {
+  status?: number;
+  constructor({
+    message = 'Api error',
+    payload,
+    success = false,
+    status,
+  }: {
+    message?: string;
+    payload?: T;
+    success?: boolean;
+    status?: number;
+  } = {}) {
     super(message);
     this.name = 'ApiError';
     this.payload = payload;
     this.success = success;
+    this.status = status;
   }
 }
