904e7f546ec3969af5d7c03f18f379ef6952d599
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-14 13:09:37 +0200

    Add in lib folder constants and messages file with server messages

---
+++ lib/constants/messages.ts
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGES = {
+  UNAUTHORIZED: 'You are not authorized to access this resource',
+  NOT_FOUND: 'The resource you are looking for could not be found',
+  INTERNAL_SERVER_ERROR: 'Something went wrong. Please try again later',
+  FORBIDDEN: 'You do not have permission to access this resource',
+};
