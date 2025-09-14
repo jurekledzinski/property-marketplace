2d22ed1a34d7e0f7d98a9dcce1b9edbfb17c3471
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-14 12:48:16 +0200

    Create deleteAdvert server action

---
+++ actions/deleteAdvert.ts
@@ -0,0 +1,55 @@
+'use server';
+import { Advert, DraftFile } from '@/models';
+import { auth } from '@/auth';
+import { uniqBy } from 'lodash';
+
+import {
+  connectDBAction,
+  DataDB,
+  deleteImagesImagekit,
+  deleteUserAdvert,
+  errorResponseAction,
+  getCollectionDb,
+  getUserAdvertImages,
+  getUserAdvertDraftImages,
+  successResponseAction,
+} from '@/lib';
+
+export const deleteAdvert = connectDBAction(
+  async (prevState: unknown, formData: FormData) => {
+    const session = await auth();
+    const { advertId } = Object.fromEntries(formData);
+
+    if (!session) return errorResponseAction('Unauthorized');
+
+    const ctx = { advertId: advertId.toString(), userId: session.user.id };
+
+    const advertsCol = getCollectionDb<DataDB<Advert>>('adverts');
+    const draftsCol = getCollectionDb<DataDB<DraftFile>>('draftImages');
+
+    if (!advertsCol || !draftsCol) {
+      return errorResponseAction('Internal server error');
+    }
+
+    const advertImages = await getUserAdvertImages(ctx, advertsCol);
+    const draftImages = await getUserAdvertDraftImages(ctx, draftsCol);
+
+    const mergedImages = uniqBy([...advertImages, ...draftImages], 'fileId');
+
+    const result = await deleteImagesImagekit({
+      checkIsOriginal: false,
+      images: mergedImages,
+      userId: session.user.id,
+    });
+
+    if (result === false) return errorResponseAction('Internal server error');
+
+    const resultDeleteAdvert = await deleteUserAdvert(ctx, advertsCol);
+
+    if (!resultDeleteAdvert) {
+      return errorResponseAction('Delete failed: No document found');
+    }
+
+    return successResponseAction('Account delete successful');
+  }
+);
