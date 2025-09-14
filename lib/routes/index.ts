14491fbdf760ca188f16deeb32040cfaeec8a2ed
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-14 13:18:02 +0200

    Update file index re exports in lib routes

--- lib/routes/index.ts
+++ lib/routes/index.ts
@@ -1,3 +1,6 @@
+export * from './advert';
+export * from './adverts';
 export * from './user-advert';
 export * from './user-adverts';
+export * from './user-messages';
 export * from './user-profile';
