e104af99ae434f151bd59f25c66867e6b18c49dd
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-14 13:18:33 +0200

    Add validation to sender key in message zod schema

--- models/message.ts
+++ models/message.ts
@@ -1,12 +1,15 @@
 import { z } from 'zod';
 
 export const MessageSchema = z.object({
-  id: z.string().optional(),
   userId: z.string(),
-  sender: z.object({ name: z.string(), email: z.email() }),
+  sender: z
+    .object({ name: z.string(), email: z.email() })
+    .refine((sender) => sender.name && sender.email, {
+      error: 'Sender must have a valid name and email',
+    }),
   title: z.string(),
   description: z.string(),
-  createdAt: z.iso.datetime(),
+  createdAt: z.date(),
 });
 
 export type Message = z.infer<typeof MessageSchema>;
