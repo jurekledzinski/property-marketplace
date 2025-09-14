ff00aa43d8dfa1cdcf26e9c50c987bfe17af7938
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-14 12:58:58 +0200

    Update user form control

--- components/pages/user-profile/hooks/control-user-form/types.ts
+++ components/pages/user-profile/hooks/control-user-form/types.ts
@@ -1,4 +1,12 @@
-export type InputsUser = {
-  name: string;
-  email: string;
+import { User } from '@/models';
+
+export type InputsUser = Omit<User, 'password'>;
+
+export type UseUserFormProps = {
+  isPending: boolean;
+  isSuccess?: boolean;
+  onSubmitForm: (payload: FormData) => void;
+  user: (InputsUser & { id: string }) | null;
+  onFailed?: () => void;
+  onSuccess?: () => void;
 };
--- components/pages/user-profile/hooks/control-user-form/useUserForm.tsx
+++ components/pages/user-profile/hooks/control-user-form/useUserForm.tsx
@@ -1,19 +1,40 @@
 'use client';
-import { InputsUser } from './types';
+import { InputsUser, UseUserFormProps } from './types';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { useCallback } from 'react';
+import { startTransition } from 'react';
+import { useResetForm } from '@/hooks';
 
 const defaultValues = {
   name: '',
   email: '',
 };
 
-export const useUserForm = () => {
+export const useUserForm = ({
+  isPending,
+  isSuccess,
+  onFailed,
+  onSubmitForm,
+  onSuccess,
+  user,
+}: UseUserFormProps) => {
   const formControl = useForm<InputsUser>({
-    defaultValues,
+    defaultValues: { ...defaultValues, ...(user && { ...user }) },
   });
 
-  const onSubmit: SubmitHandler<InputsUser> = useCallback((data) => {}, []);
+  const onSubmit: SubmitHandler<InputsUser> = (data) => {
+    const formData = new FormData();
+    formData.set('email', data.email);
+    formData.set('name', data.name);
+    startTransition(() => onSubmitForm(formData));
+  };
+
+  useResetForm({
+    isPending,
+    isSuccess,
+    formControl,
+    onFailed,
+    onSuccess,
+  });
 
   return { formControl, onSubmit };
 };
