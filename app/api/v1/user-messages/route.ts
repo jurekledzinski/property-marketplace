24411fab68adc41d864e6a4a4a17c1f1d509e9ed
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-14 12:52:23 +0200

    Create api route user messages

---
+++ app/api/v1/user-messages/route.ts
@@ -0,0 +1,35 @@
+import 'server-only';
+import { auth } from '@/auth';
+import { Message } from '@/models';
+
+import {
+  connectDBAuth,
+  DataDB,
+  errorResponseApi,
+  formatDBDocumentId,
+  getCollectionDb,
+  getUserMessages,
+  successResponseApi,
+} from '@/lib';
+
+export const GET = connectDBAuth(
+  auth(async (req) => {
+    if (!req.auth) {
+      return errorResponseApi({ message: 'Unauthorized', status: 401 });
+    }
+
+    const ctx = { userId: req.auth.user.id };
+
+    const messagesCol = getCollectionDb<DataDB<Message>>('messages');
+
+    if (!messagesCol) {
+      return errorResponseApi({ message: 'Internal server error' });
+    }
+
+    const messages = await getUserMessages(ctx, messagesCol);
+
+    const result = formatDBDocumentId(messages);
+
+    return successResponseApi({ payload: result });
+  })
+);
