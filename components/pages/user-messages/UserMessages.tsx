10baba16fe208d3c01883562331c617b53d5ebf3
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-15 19:33:38 +0200

    Refactor userMessages

--- components/pages/user-messages/UserMessages.tsx
+++ components/pages/user-messages/UserMessages.tsx
@@ -1,39 +1,87 @@
 'use client';
 import styles from './UserMessages.module.css';
-import { Box, Button, ButtonGroup, Heading } from '@/components';
+import { deleteMessage } from '@/actions';
 import { MessageAccordion } from './components';
+import { modalMessages } from '@/constants';
+import { showSuccessToast } from '@/helpers';
+import { startTransition } from 'react';
+import { useActionStateReset } from '@/hooks';
 import { useControlAccordion } from './hooks';
 import { UserMessagesProps } from './types';
 
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Heading,
+  Modal,
+  useControlModal,
+} from '@/components';
+
 export const UserMessages = ({ messages = [] }: UserMessagesProps) => {
-  const { handleSelect, handleDelete, selectedIds } = useControlAccordion();
+  const action = useActionStateReset({ fnAction: deleteMessage });
+  const accordion = useControlAccordion();
+  const modal = useControlModal();
+  const { description, title } = modalMessages.deleteMessages(
+    accordion.deleteIds.length
+  );
 
   return (
     <>
       <Heading level={4} mb="mb-md" mt="mt-sm">
         Messages
       </Heading>
-
       <ButtonGroup mt="mt-md" mb="mb-md" justify="justify-end" fullWidth>
         <Button
           color="negative"
+          disabled={!accordion.deleteIds.length}
           label="Delete messages"
+          onClick={modal.onOpen}
           size="size-md"
           variant="outlined"
         />
       </ButtonGroup>
-
       <Box className={styles.container}>
         {messages.map((message) => (
           <MessageAccordion
-            selectedIds={selectedIds}
+            openIds={accordion.openIds}
             key={message.id}
             message={message}
-            onChangeDelete={handleDelete}
-            onClick={handleSelect}
+            onChangeDelete={accordion.handleDelete}
+            onClick={accordion.handleSelect}
           />
         ))}
       </Box>
+      <Modal
+        confirmText="Delete"
+        title="Delete messages"
+        isPending={action.isPending}
+        isOpen={modal.isOpen}
+        isSuccess={action.state.success}
+        onCancel={modal.onClose}
+        onClose={modal.onClose}
+        onConfirm={() => {
+          startTransition(() => {
+            const formData = new FormData();
+            formData.append('deleteIds', JSON.stringify(accordion.deleteIds));
+            action.formAction(formData);
+          });
+        }}
+        onFailed={() => {
+          showSuccessToast('Not all messages were deleted successfully');
+        }}
+        onSuccess={async () => {
+          showSuccessToast('Messages deleted successfully');
+          modal.onClose();
+          action.resetStateAction();
+          accordion.handleClearDeleteIds();
+        }}
+        variant="negative"
+      >
+        {title}
+        <br />
+        {description}
+      </Modal>
     </>
   );
 };
