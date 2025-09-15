b9103fe0fcdb65e12863bd21254d5680423ca4e0
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-15 13:32:01 +0200

    Add more server endpoints and format object serverEndpoints

--- lib/fetch-api/utils/serverEndpoints.ts
+++ lib/fetch-api/utils/serverEndpoints.ts
@@ -3,9 +3,15 @@
 export const serverEndpoints = {
   advert: (domain: string, advertId?: string) =>
     `${domain}/api/v1/advert?id=${advertId}`,
+
   adverts: (domain: string) => `${domain}/api/v1/adverts`,
+
+  userMessages: (domain: string) => `${domain}/api/v1/user-messages`,
+
   userAdverts: (domain: string) => `${domain}/api/v1/user-adverts`,
+
   userAdvert: (domain: string, advertId?: string) =>
     `${domain}/api/v1/user-advert?id=${advertId}`,
+
   user: (domain: string) => `${domain}/api/v1/user-profile`,
 };
