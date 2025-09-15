ceac4b8650a7193b9a9f983af78e95111e37d710
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-15 12:22:17 +0200

    Update useContactForm hook

--- components/shared/contact-form/hooks/contact-form/useContactForm.tsx
+++ components/shared/contact-form/hooks/contact-form/useContactForm.tsx
@@ -1,17 +1,45 @@
 'use client';
+import { useResetForm } from '@/hooks';
 import { InputsContact, useContactFormProps } from './types';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { startTransition } from 'react';
 
 const defaultValues = {
   name: '',
   email: '',
   message: '',
+  title: '',
 };
 
-export const useContactForm = ({ userId }: useContactFormProps) => {
-  const formControls = useForm<InputsContact>({ defaultValues });
+export const useContactForm = ({
+  isPending,
+  isSuccess,
+  onFailed,
+  onSubmitForm,
+  onSuccess,
+  userId,
+}: useContactFormProps) => {
+  const formControl = useForm<InputsContact>({ defaultValues });
 
-  const onSubmit: SubmitHandler<InputsContact> = (data) => {};
+  const onSubmit: SubmitHandler<InputsContact> = (data) => {
+    const formData = new FormData();
+    formData.set('name', data.name);
+    formData.set('email', data.email);
+    formData.set('message', data.message);
+    formData.set('title', data.title);
+    formData.set('userId', userId);
 
-  return { formControls, onSubmit };
+    startTransition(() => onSubmitForm(formData));
+  };
+
+  useResetForm({
+    isPending,
+    isSuccess,
+    formControl,
+    defaultValues,
+    onFailed,
+    onSuccess,
+  });
+
+  return { formControl, onSubmit };
 };
