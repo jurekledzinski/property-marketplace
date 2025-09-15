e010b7da5157f8041270d25a829c0faacf9d72c5
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-15 13:31:02 +0200

    Create helper getMessagesPage for user messages

---
+++ lib/fetch-api/user-messages/messages.ts
@@ -0,0 +1,21 @@
+import 'server-only';
+import { fetchApi, getDomain, ReadonlyHeaders, serverEndpoints } from '@/lib';
+import { Messages } from './types';
+
+// Fetch all user messages server component
+
+export const getMessagesPage = async (headers?: ReadonlyHeaders) => {
+  const domain = await getDomain();
+
+  const response = await fetchApi<Messages[]>({
+    tags: ['messages'],
+    url: serverEndpoints.userMessages(domain),
+    headers,
+  });
+
+  if (!response.success || !response.payload) {
+    return null;
+  }
+
+  return response.payload;
+};
