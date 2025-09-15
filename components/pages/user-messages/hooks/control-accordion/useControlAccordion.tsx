17ad0dd2f1ceec550a2696c071e3f4b2dcf25966
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-15 19:35:32 +0200

    Refactor names local state in useControlAccordion and add state to keep deleted ids messages

--- components/pages/user-messages/hooks/control-accordion/useControlAccordion.tsx
+++ components/pages/user-messages/hooks/control-accordion/useControlAccordion.tsx
@@ -1,21 +1,29 @@
 'use client';
+import { selectAccordion } from './utils';
 import { useState } from 'react';
 import type { ChangeEvent, MouseEvent } from 'react';
 
 export const useControlAccordion = () => {
-  const [selectedIds, setSelectedIds] = useState<string[]>([]);
+  const [openIds, setOpenIds] = useState<string[]>([]);
+  const [deleteIds, setDeleteIds] = useState<string[]>([]);
 
   const handleDelete = (e: ChangeEvent<HTMLInputElement>) => {
-    const id = (e.target as HTMLInputElement).id;
+    const id = (e.target as HTMLInputElement).id.replace('delete-', '');
+    setDeleteIds((prev) => selectAccordion(prev, id));
   };
 
   const handleSelect = (e: MouseEvent<HTMLInputElement>) => {
     const id = (e.target as HTMLInputElement).id;
-    setSelectedIds((prev) => {
-      if (prev.includes(id)) return [...prev].filter((i) => i !== id);
-      return [...prev, id];
-    });
+    setOpenIds((prev) => selectAccordion(prev, id));
   };
 
-  return { selectedIds, handleSelect, handleDelete };
+  const handleClearDeleteIds = () => setDeleteIds([]);
+
+  return {
+    deleteIds,
+    openIds,
+    handleSelect,
+    handleClearDeleteIds,
+    handleDelete,
+  };
 };
