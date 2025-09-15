c5f569d3b363bd1e29f701a52601b43d84d929c4
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-15 12:22:01 +0200

    Add props for contact form

--- components/shared/contact-form/types.ts
+++ components/shared/contact-form/types.ts
@@ -5,5 +5,6 @@ import { SubmitHandler, UseFormReturn } from 'react-hook-form';
 export interface ContactFormProps
   extends Omit<FormHTMLAttributes<HTMLFormElement>, 'onSubmit'> {
   controls: UseFormReturn<InputsContact, unknown, InputsContact>;
+  isPending: boolean;
   onSubmit: SubmitHandler<InputsContact>;
 }
