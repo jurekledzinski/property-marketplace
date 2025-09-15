52970d92af29314e1e53d358111dc2d36c512ac4
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-15 12:21:52 +0200

    Add props for contact form

--- components/shared/contact-form/hooks/contact-form/types.ts
+++ components/shared/contact-form/hooks/contact-form/types.ts
@@ -1,9 +1,12 @@
-export type InputsContact = {
-  name: string;
-  email: string;
-  message: string;
-};
+import { Message } from '@/models';
+
+export type InputsContact = Omit<Message, 'createdAt' | 'userId'>;
 
 export type useContactFormProps = {
+  isPending: boolean;
+  onSubmitForm: (payload: FormData) => void;
   userId: string;
+  isSuccess?: boolean;
+  onFailed?: () => void;
+  onSuccess?: () => void;
 };
