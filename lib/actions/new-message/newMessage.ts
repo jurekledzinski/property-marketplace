fdac2d6b29327408934ffc747b2ab9062e03e221
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-15 12:24:04 +0200

    Update helper function formatDataNewMessage used in server action newMessage

--- lib/actions/new-message/newMessage.ts
+++ lib/actions/new-message/newMessage.ts
@@ -1,14 +1,7 @@
 export const formatDataNewMessage = (formData: FormData) => {
   const data = Object.fromEntries(formData);
-  const sender = formData.get('sender')
-    ? JSON.parse(formData.get('sender') as string)
-    : {};
 
-  const dataForm = {
-    ...data,
-    sender,
-    createdAt: new Date(),
-  };
+  const dataForm = { ...data, createdAt: new Date() };
 
   return dataForm;
 };
