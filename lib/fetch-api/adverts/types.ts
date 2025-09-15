b1132e528cb3c1a2ff2ce1a8739a0097c159fc28
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-15 09:43:56 +0200

    Update types for helpers used to fetch adverts in server components

--- lib/fetch-api/adverts/types.ts
+++ lib/fetch-api/adverts/types.ts
@@ -1,9 +1,28 @@
 import { Advert } from '@/models';
 
-export interface UserAdvertsTable
-  extends Pick<Advert, 'userId' | 'title' | 'type'> {
+export type PickInUserAdvert = 'userId' | 'title' | 'type';
+
+export interface UserAdvertsTable extends Pick<Advert, PickInUserAdvert> {
   id: string;
   stage?: string;
   actions?: string;
   createdAt?: string;
+}
+
+export type OmitInAdverts =
+  | 'advertiser'
+  | 'amenities'
+  | 'condition'
+  | 'description'
+  | 'email'
+  | 'files'
+  | 'images'
+  | 'phone'
+  | 'style'
+  | 'type'
+  | 'year';
+
+export interface Adverts extends Omit<Advert, OmitInAdverts> {
+  id: string;
+  image: Advert['images'][0];
 }
