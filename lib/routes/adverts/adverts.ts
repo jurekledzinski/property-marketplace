96acd2d1dc86ce549e6136c01b5dbaad596cc8b8
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-15 09:45:15 +0200

    Create helper function getAdverts used in api route adverts

--- lib/routes/adverts/adverts.ts
+++ lib/routes/adverts/adverts.ts
@@ -5,7 +5,34 @@ import { DataDB } from '@/lib';
 
 // --- GET ---
 
+const projection = {
+  advertiser: 0,
+  amenities: 0,
+  condition: 0,
+  description: 0,
+  email: 0,
+  files: 0,
+  images: 0,
+  phone: 0,
+  style: 0,
+  type: 0,
+  year: 0,
+};
+
 export const getAdverts = async (col: Collection<DataDB<Advert>>) => {
-  const adverts = await col.find<DataDB<Advert>>({}).toArray();
+  const adverts = await col
+    .aggregate<DataDB<Advert>>([
+      {
+        $addFields: {
+          image: { $first: '$images' },
+        },
+      },
+      {
+        $project: {
+          ...projection,
+        },
+      },
+    ])
+    .toArray();
   return adverts;
 };
