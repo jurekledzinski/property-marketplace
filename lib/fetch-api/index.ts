a8e138d22b674c74f68503b767089f92420e19e8
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-16 18:43:57 +0200

    Create fetch api helper function getUserAnalyticsPage

--- lib/fetch-api/index.ts
+++ lib/fetch-api/index.ts
@@ -1,4 +1,5 @@
 export * from './adverts';
+export * from './user-analytics';
 export * from './user-messages';
 export * from './user-profile';
 export * from './utils';
---
+++ lib/fetch-api/user-analytics/userAnalytics.ts
@@ -0,0 +1,19 @@
+import 'server-only';
+import { DashboardAnalytics } from '@/models';
+import { fetchApi, getDomain, ReadonlyHeaders, serverEndpoints } from '@/lib';
+
+export const getUserAnalyticsPage = async (headers?: ReadonlyHeaders) => {
+  const domain = await getDomain();
+
+  const response = await fetchApi<DashboardAnalytics>({
+    tags: ['user-analytics'],
+    url: serverEndpoints.userAnalytics(domain),
+    headers,
+  });
+
+  if (!response.success || !response.payload) {
+    return null;
+  }
+
+  return response.payload;
+};
