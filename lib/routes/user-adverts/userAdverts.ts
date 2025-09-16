2884393442d53809f18f767a1180346de9d19c1c
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-16 18:46:09 +0200

    Change name for GetUserAdvertsContext in api user adverts

--- lib/routes/user-adverts/userAdverts.ts
+++ lib/routes/user-adverts/userAdverts.ts
@@ -2,12 +2,12 @@ import 'server-only';
 import { Advert } from '@/models';
 import { Collection } from 'mongodb';
 import { DataDB, UserAdvertsTable } from '@/lib';
-import { GETUserAdvertsContext } from './types';
+import { GetUserAdvertsContext } from './types';
 
 // --- GET ---
 
 export const getUserAdverts = async (
-  ctx: GETUserAdvertsContext,
+  ctx: GetUserAdvertsContext,
   col: Collection<Advert>
 ) => {
   const { userId } = ctx;
