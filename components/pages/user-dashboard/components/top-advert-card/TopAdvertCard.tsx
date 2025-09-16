aa50be1e5821b28e322311c3c84cb3e287939dfb
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-16 18:40:58 +0200

    Add types for TopAdvertCard

--- components/pages/user-dashboard/components/top-advert-card/TopAdvertCard.tsx
+++ components/pages/user-dashboard/components/top-advert-card/TopAdvertCard.tsx
@@ -1,16 +1,17 @@
 import styles from '../../UserDashboard.module.css';
 import { Card, CardContent, CardFooter, CardHeader, Icon } from '@/components';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { TopAdvertCardProps } from './types';
 
-export const TopAdvertCard = () => {
+export const TopAdvertCard = ({ title }: TopAdvertCardProps) => {
   return (
     <Card className={styles.topAdvert}>
       <CardContent className={styles.cardContent}>
-        <p style={{ fontSize: 20, fontWeight: 500 }}>14</p>
-        <p style={{ fontSize: 14 }}>Top viewed advert</p>
+        <p className={styles.cardTitle}>{title}</p>
+        <p className={styles.cardText}>Top viewed advert</p>
       </CardContent>
       <CardHeader className={styles.cardHeader}>
-        <Icon icon={faEye} size="2x" />
+        <Icon icon={faEye} size="1x" />
       </CardHeader>
       <CardFooter className={styles.cardFooter}></CardFooter>
     </Card>
--- components/pages/user-dashboard/components/total-adverts-card/index.ts
+++ components/pages/user-dashboard/components/total-adverts-card/index.ts
@@ -1,1 +1,2 @@
 export * from './TotalAdvertsCard';
+export * from './types';
