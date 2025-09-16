bc40d6226e1fb2674ee40f89fb0c16c3f53bad6f
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-16 18:45:00 +0200

    Update server endpoints object add userAnalytics endpoint

--- lib/routes/index.ts
+++ lib/routes/index.ts
@@ -2,5 +2,6 @@ export * from './advert';
 export * from './adverts';
 export * from './user-advert';
 export * from './user-adverts';
+export * from './user-analytics';
 export * from './user-messages';
 export * from './user-profile';
