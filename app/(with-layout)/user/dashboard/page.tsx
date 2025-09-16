1f1890d5da658c326a208c288db3033dd1f8ae9a
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-16 18:14:59 +0200

    Add fetch userAnalytics data for user dashboard

--- app/(with-layout)/user/dashboard/page.tsx
+++ app/(with-layout)/user/dashboard/page.tsx
@@ -1,21 +1,12 @@
 import { DashboardGrid } from '@/components';
+import { getUserAnalyticsPage } from '@/lib';
+import { headers } from 'next/headers';
 
-const serverData = {
-  topAdvert: 'Advert 1',
-  total: { adverts: 12, messages: 3, rent: 3, sale: 1 },
-  views: [
-    { label: 'Modern house with 3 bedrooms', amount: 4 },
-    { label: 'Cousy traditional apartment in center old town', amount: 2 },
-    { label: 'Modern house in city', amount: 8 },
-  ],
-  stats: [
-    { label: 'Published', amount: 4 },
-    { label: 'InActive', amount: 8 },
-  ],
-};
+const UserDashboardPage = async () => {
+  const headersData = await headers();
+  const analytics = await getUserAnalyticsPage(headersData);
 
-const UserDashboardPage = () => {
-  return <DashboardGrid dashboardAnalytics={serverData} />;
+  return <DashboardGrid analytics={analytics} />;
 };
 
 export default UserDashboardPage;
