8213981592d114ae2d1aa8ba360d145151f75134
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-17 20:25:47 +0200

    Update adverts GET route provider search params

--- app/api/v1/adverts/route.ts
+++ app/api/v1/adverts/route.ts
@@ -1,25 +1,38 @@
 import 'server-only';
+import { Advert } from '@/models';
+
 import {
   connectDB,
   DataDB,
+  decodeQueryUrl,
   errorResponseApi,
   formatDBDocumentId,
   getAdverts,
   getCollectionDb,
+  getQueries,
   successResponseApi,
+  GetAdvertsSearchParams,
 } from '@/lib';
-import { Advert } from '@/models';
 
-export const GET = connectDB(async () => {
+export const GET = connectDB(async (req) => {
+  const searchParams = getQueries<GetAdvertsSearchParams>(req);
+
+  const decodedParams = decodeQueryUrl(searchParams, ['amenities']);
+
   const advertsCol = getCollectionDb<DataDB<Advert>>('adverts');
 
   if (!advertsCol) {
     return errorResponseApi({ message: 'Internal server error' });
   }
 
-  const adverts = await getAdverts(advertsCol);
+  const adverts = await getAdverts(advertsCol, decodedParams);
 
-  const result = formatDBDocumentId(adverts);
+  const result = formatDBDocumentId(adverts.data);
 
-  return successResponseApi({ payload: result });
+  return successResponseApi({
+    payload: {
+      data: result,
+      totalItems: adverts.totalItems,
+    },
+  });
 });
