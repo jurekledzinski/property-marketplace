bc1d7c4224b4d4b88b99389584211567c8befcd5
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-22 21:29:53 +0200

    Create helper function getStage for adverts table

--- components/pages/user-adverts/components/adverts-table/index.ts
+++ components/pages/user-adverts/components/adverts-table/index.ts
@@ -2,3 +2,4 @@ export * from './AdvertsTable';
 export * from './components';
 export * from './hooks';
 export * from './types';
+export * from './utils';
---
+++ components/pages/user-adverts/components/adverts-table/utils/stageAdverts.ts
@@ -0,0 +1,18 @@
+import { UserAdvertsTable } from '@/lib';
+
+const maxDays = 7 * 24 * 60 * 60 * 1000;
+const minDays = 5 * 24 * 60 * 60 * 1000;
+
+export const getStage = (updatedAt: UserAdvertsTable['updatedAt']) => {
+  if (!updatedAt) return;
+  const updateDate = new Date(updatedAt);
+  const age = Date.now() - updateDate.getTime();
+
+  if (age >= minDays && age <= maxDays) {
+    return 'soon expire';
+  } else if (age < minDays) {
+    return 'active';
+  } else if (age > maxDays) {
+    return 'inActive';
+  }
+};
