4ae73583b826b8d65f47fc0dd2d5094640efe31e
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-22 15:24:52 +0200

    Update bgColors

--- components/shared/chart-bar/utils/bgColors.ts
+++ components/shared/chart-bar/utils/bgColors.ts
@@ -1,26 +1,26 @@
 export const bgColors = [
-  'rgba(255, 99, 132, 0.4)', // red
-  'rgba(54, 162, 235, 0.4)', // blue
-  'rgba(255, 206, 86, 0.4)', // yellow
-  'rgba(75, 192, 192, 0.4)', // teal
-  'rgba(153, 102, 255, 0.4)', // purple
-  'rgba(255, 159, 64, 0.4)', // orange
-  'rgba(199, 199, 199, 0.4)', // gray
-  'rgba(255, 99, 71, 0.4)', // tomato
-  'rgba(100, 181, 246, 0.4)', // light blue
-  'rgba(255, 138, 101, 0.4)', // soft orange
-  'rgba(244, 143, 177, 0.4)', // pink
-  'rgba(174, 213, 129, 0.4)', // lime
-  'rgba(255, 213, 79, 0.4)', // amber
-  'rgba(77, 182, 172, 0.4)', // cyan/teal
-  'rgba(255, 112, 67, 0.4)', // deep orange
-  'rgba(144, 164, 174, 0.4)', // blue gray
-  'rgba(233, 30, 99, 0.4)', // strong pink
-  'rgba(121, 134, 203, 0.4)', // indigo
-  'rgba(205, 220, 57, 0.4)', // chartreuse
-  'rgba(129, 199, 132, 0.4)', // light green
-  'rgba(252, 169, 3, 0.4)', // light orange
-  'rgba(199, 21, 133, 0.4)', // light violet
-  'rgba(255, 65, 54, 0.4)', // light red
-  'rgba(46, 204, 64, 0.4)', // light green
+  'rgba(255, 99, 132, 1)', // red
+  'rgba(54, 162, 235, 1)', // blue
+  'rgba(255, 206, 86, 1)', // yellow
+  'rgba(75, 192, 192, 1)', // teal
+  'rgba(153, 102, 255, 1)', // purple
+  'rgba(255, 159, 64, 1)', // orange
+  'rgba(199, 199, 199, 1)', // gray
+  'rgba(255, 99, 71, 1)', // tomato
+  'rgba(100, 181, 246, 1)', // light blue
+  'rgba(255, 138, 101, 1)', // soft orange
+  'rgba(244, 143, 177, 1)', // pink
+  'rgba(174, 213, 129, 1)', // lime
+  'rgba(255, 213, 79, 1)', // amber
+  'rgba(77, 182, 172, 1)', // cyan/teal
+  'rgba(255, 112, 67, 1)', // deep orange
+  'rgba(144, 164, 174, 1)', // blue gray
+  'rgba(233, 30, 99, 1)', // strong pink
+  'rgba(121, 134, 203, 1)', // indigo
+  'rgba(205, 220, 57, 1)', // chartreuse
+  'rgba(129, 199, 132, 1)', // light green
+  'rgba(199, 21, 133, 1)', // light violet
+  'rgba(252, 169, 3, 1)', // light orange
+  'rgba(255, 65, 54, 1)', // light red
+  'rgba(46, 204, 64, 1)', // light green
 ];
