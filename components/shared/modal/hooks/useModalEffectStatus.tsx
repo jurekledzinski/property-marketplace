c1859592a16bb7e715216dfb94c765078bd2cc4b
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-23 14:32:29 +0200

    Add in useModalEffectStatus add class to body when open or remove when close modal

--- components/shared/modal/hooks/useModalEffectStatus.tsx
+++ components/shared/modal/hooks/useModalEffectStatus.tsx
@@ -10,6 +10,9 @@ export const useModalEffectStatus = ({
   isOpen,
 }: UseControlModalProps) => {
   useEffect(() => {
+    if (isOpen) document.body.classList.add('modal-open');
+    else document.body.classList.remove('modal-open');
+
     if (isOpen && isSuccess && !isPending && onSuccess) onSuccess();
     if (isOpen && isSuccess === false && !isPending && onFailed) onFailed();
   }, [isPending, isSuccess, isOpen, onFailed, onSuccess]);
