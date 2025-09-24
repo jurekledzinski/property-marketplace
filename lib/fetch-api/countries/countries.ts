117a527c5062a14735ef9127baf14dd1be98e856
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-24 20:46:49 +0200

    Create helper folder for countries in fetch api lib

---
+++ lib/fetch-api/countries/countries.ts
@@ -0,0 +1,18 @@
+import { fetchApi, serverEndpoints } from '@/lib';
+import { Country } from './types';
+
+// Fetch countries server component
+
+const headers = new Headers();
+headers.append('Content-Type', 'application/json');
+headers.append('Authorization', `Bearer ${process.env.API_COUNTRIES_KEY}`);
+
+export const getCountries = async () => {
+  const response = await fetchApi<Country[]>({
+    url: serverEndpoints.countries(),
+    cache: 'force-cache',
+    headers,
+  });
+
+  return response as unknown as Country[];
+};
