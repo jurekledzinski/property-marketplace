8e7ed0c94038435bcdcf1ed116a36095fc2e4c91
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-24 20:45:17 +0200

    Create folder for fetch api cities in lib folder

--- lib/fetch-api/index.ts
+++ lib/fetch-api/index.ts
@@ -1,4 +1,6 @@
 export * from './adverts';
+export * from './countries';
+export * from './states';
 export * from './user-analytics';
 export * from './user-messages';
 export * from './user-profile';
