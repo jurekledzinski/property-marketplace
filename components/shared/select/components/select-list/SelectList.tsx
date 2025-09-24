254821a7d9b957de242a12bf15a898acfafe7296
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-24 20:41:34 +0200

    Add forward ref in SelectList component

--- components/shared/select/components/select-list/SelectList.tsx
+++ components/shared/select/components/select-list/SelectList.tsx
@@ -1,8 +1,19 @@
+'use client';
+import React, { forwardRef } from 'react';
+import styles from '../../Select.module.css';
 import { SelectListProps } from './types';
 import { useAriaAttributes } from '@/hooks';
 
-export const SelectList = ({ children }: SelectListProps) => {
-  const a11y = useAriaAttributes().selectListA11y();
+export const SelectList = forwardRef<HTMLDivElement, SelectListProps>(
+  ({ children }, ref) => {
+    const a11y = useAriaAttributes().selectListA11y();
 
-  return <div {...a11y}>{children}</div>;
-};
+    return (
+      <div {...a11y} ref={ref} className={styles.selectList}>
+        {children}
+      </div>
+    );
+  }
+);
+
+SelectList.displayName = 'SelectList';
