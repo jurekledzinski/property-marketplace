650464f34b16c7d09f7b29568ffd5c1901970122
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-25 16:12:50 +0200

    Remove comments in select list types

--- components/shared/select/components/select-list/types.ts
+++ components/shared/select/components/select-list/types.ts
@@ -1,6 +1,1 @@
 export type SelectListProps = React.HTMLAttributes<HTMLDivElement>;
-
-// T to option
-// export type SelectPanelProps<T> =
-//   | { children?: React.ReactNode }
-//   | { children: (item: T) => React.ReactNode };
