43c5c1b1a3177f0878a90ed6c83e3c5a489470f5
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-27 14:45:50 +0200

    Add closeOnScroll prop to select props

--- components/shared/select/types.ts
+++ components/shared/select/types.ts
@@ -1,15 +1,19 @@
 import { Size, Variant } from '@/types';
 
-export type Option = { key: string; value: string };
+export type Option = {
+  key: string;
+  value: string;
+};
 type PopOverVariant = Exclude<Variant, 'text'> | 'basic' | 'underline';
 
 export type SelectProps = {
   children?: React.ReactNode;
+  closeOnScroll?: boolean;
   gap?: number;
   isError?: boolean;
   label?: string;
-  size?: Size;
-  variant?: PopOverVariant;
-  value?: string;
   onChange?: (key: string, value?: Record<string, string>) => void;
+  size?: Size;
+  value?: string;
+  variant?: PopOverVariant;
 };
