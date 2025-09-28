ad8713067f0bc17fcb75ddbc42be56dab2a488af
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-28 20:09:09 +0200

    Add onClose popover when click select option

--- components/shared/select/components/select-option/SelectOption.tsx
+++ components/shared/select/components/select-option/SelectOption.tsx
@@ -1,12 +1,13 @@
 import { selectOptionClassNames } from '../../utils';
 import { SelectOptionProps } from './types';
 import { useAriaAttributes } from '@/hooks';
+import { usePopOver } from '@/components';
 import { useSelect } from '../../store';
 
 export const SelectOption = ({ children, data, id }: SelectOptionProps) => {
   const { onChange, size, value } = useSelect();
+  const { onClose } = usePopOver();
   const a11y = useAriaAttributes().selectOptionA11y(id, value === id);
-
   const classes = selectOptionClassNames(id, value, size);
 
   return (
@@ -14,7 +15,10 @@ export const SelectOption = ({ children, data, id }: SelectOptionProps) => {
       data-id={id}
       data-type="item"
       className={classes.selectOption}
-      onClick={() => onChange && onChange(id, data)}
+      onClick={() => {
+        if (onChange) onChange(id, data);
+        onClose('root');
+      }}
       tabIndex={0}
       {...a11y}
     >
