eb2e9cc9f90cae387600205d6f6705958eeb6fe5
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-29 14:28:05 +0200

    Create file types in user new advert folder

--- components/pages/user-new-advert/index.ts
+++ components/pages/user-new-advert/index.ts
@@ -1,4 +1,5 @@
 export * from './components';
 export * from './hooks';
+export * from './types';
 export * from './UserNewAdvert';
 export * from './utils';
---
+++ components/pages/user-new-advert/types.ts
@@ -0,0 +1,5 @@
+import { Countries } from '@/lib';
+
+export type UserNewAdvertProps = {
+  countries: Countries;
+};
