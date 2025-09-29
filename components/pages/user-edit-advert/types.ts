5cc0f13be384646559a390f6e771c7de050c8f48
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-29 14:20:51 +0200

    Add type for countries data to UserEditAdvert component

--- components/pages/user-edit-advert/types.ts
+++ components/pages/user-edit-advert/types.ts
@@ -1,5 +1,7 @@
+import { Countries } from '@/lib';
 import { InputsAvert } from '../user-new-advert';
 
 export type UserEditAdvertProps = {
   advert: InputsAvert | null;
+  countries: Countries;
 };
