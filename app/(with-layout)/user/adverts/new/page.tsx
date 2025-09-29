7ddf3e961ef2477f84ad6c79ab42994cc5fda032
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-29 14:18:21 +0200

    Add fetch countries in NewAdvertPage

--- app/(with-layout)/user/adverts/new/page.tsx
+++ app/(with-layout)/user/adverts/new/page.tsx
@@ -1,7 +1,11 @@
+import { formatCountires, getCountries } from '@/lib';
 import { UserNewAdvert } from '@/components';
 
 const NewAdvertPage = async () => {
-  return <UserNewAdvert />;
+  const countriesData = await getCountries();
+  const countries = formatCountires(countriesData);
+
+  return <UserNewAdvert countries={countries} />;
 };
 
 export default NewAdvertPage;
