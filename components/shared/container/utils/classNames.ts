b23ab9294ccd1ac5e26b55d672fbf68301dec310
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-29 14:30:53 +0200

    Simplify destructuring assignment params in getClassesContainer

--- components/shared/container/utils/classNames.ts
+++ components/shared/container/utils/classNames.ts
@@ -4,35 +4,9 @@ import { ClassesContainer } from './types';
 import { classNames, generateClassNames, spacingClasses } from '@/helpers';
 
 export const getClassesContainer: ClassesContainer = (params) => {
-  const {
-    className,
-    m,
-    mb,
-    ml,
-    mr,
-    mt,
-    maxWidth = 'mw-md',
-    p,
-    pb,
-    pl,
-    pr,
-    pt,
-  } = params;
+  const { className, maxWidth = 'mw-md', ...rest } = params;
 
-  const spacing = spacingClasses({
-    m,
-    mb,
-    ml,
-    mr,
-    mt,
-    p,
-    pb,
-    pl,
-    pr,
-    pt,
-    maxWidth,
-  });
-
+  const spacing = spacingClasses({ ...rest, maxWidth });
   const mergedClasses = { ...styles, ...stylesSpace };
 
   return classNames(
