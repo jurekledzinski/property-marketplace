a54e81741b685c67c80e639c6c9e3f217a0f032d
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-09-29 14:44:25 +0200

    Remove unnecessary capitalize helper function in useFormatDetails

--- components/pages/details-advert/components/property-details/hooks/format-details/useFormatDetails.tsx
+++ components/pages/details-advert/components/property-details/hooks/format-details/useFormatDetails.tsx
@@ -1,4 +1,4 @@
-import { capitalizeFirstLetter, formatNumber } from '@/helpers';
+import { formatNumber } from '@/helpers';
 import { optionsFormat } from '@/components';
 import { UseFormatDetailsProps } from './types';
 import { useMemo } from 'react';
@@ -9,14 +9,12 @@ export const useFormatDetails = ({ details }: UseFormatDetailsProps) => {
     const price = formatNumber(priceValue, 'nl-NL', optionsFormat).format;
 
     return {
-      Location: `${capitalizeFirstLetter(
-        details.country
-      )}, ${capitalizeFirstLetter(details.city)}`,
-      Type: capitalizeFirstLetter(details.type),
+      Location: `${details.country}, ${details.state}, ${details.city}`,
+      Type: details.type,
       Area: `${details.area.toString()}m²`,
-      Bedrooms: capitalizeFirstLetter(details.rooms.toString()),
-      Bathrooms: capitalizeFirstLetter(details.bathrooms.toString()),
-      Status: capitalizeFirstLetter(details.status),
+      Bedrooms: details.rooms.toString(),
+      Bathrooms: details.bathrooms.toString(),
+      Status: details.status,
       Price: details.status === 'rent' ? `${price}/month` : price,
     };
   }, [details]);
