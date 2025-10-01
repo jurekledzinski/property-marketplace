18f80743bc9c59583dede0767ce0d34eea25f28b
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-01 13:24:19 +0200

    Refactor Modal component to add possibilty display modal in portal or not

--- components/shared/modal/Modal.tsx
+++ components/shared/modal/Modal.tsx
@@ -1,33 +1,31 @@
 'use client';
 import { Backdrop } from '@/components';
+import { createPortal } from 'react-dom';
 import { CSSTransition } from 'react-transition-group';
-import { ModalBody, ModalFooter, ModalHeader } from './components';
 import { modalClassNames } from './utils';
+import { ModalLayout } from './components';
 import { ModalProps } from './types';
+import { useCheckMount } from '@/hooks';
 import { useModalEffectStatus } from './hooks';
 import { useRef, useState } from 'react';
 
 export const Modal = ({
   children,
-  confirmText = 'Confirm',
-  title = 'Modal title',
-  cancelText = 'Cancel',
-  isOpen = false,
+  open = false,
   isPending = false,
   isSuccess,
-  onCancel,
-  onClose,
-  onConfirm,
   onFailed,
   onSuccess,
-  type = 'button',
-  variant = 'primary',
+  portal,
+  ...props
 }: ModalProps) => {
   const nodeRef = useRef(null);
   const [showBackdrop, setShowBackdrop] = useState(false);
 
+  const mounted = useCheckMount();
+
   useModalEffectStatus({
-    isOpen,
+    open,
     isPending,
     isSuccess,
     onFailed,
@@ -36,32 +34,42 @@ export const Modal = ({
 
   const classes = modalClassNames();
 
+  const modalElement = (
+    <CSSTransition
+      in={open}
+      nodeRef={nodeRef}
+      timeout={300}
+      classNames={classes.modal}
+      unmountOnExit
+      onEnter={() => setShowBackdrop(true)}
+      onExited={() => setShowBackdrop(false)}
+    >
+      <ModalLayout
+        className={classes.modalElement}
+        isPending={isPending}
+        ref={nodeRef}
+        {...props}
+      >
+        {children}
+      </ModalLayout>
+    </CSSTransition>
+  );
+
+  if (!mounted) return null;
+
+  if (portal) {
+    return (
+      <>
+        <Backdrop open={showBackdrop} portal={portal} zIndex={200} />
+        {createPortal(modalElement, document.body)}
+      </>
+    );
+  }
+
   return (
     <>
-      <Backdrop open={showBackdrop} />
-      <CSSTransition
-        in={isOpen}
-        nodeRef={nodeRef}
-        timeout={300}
-        classNames={classes.modal}
-        unmountOnExit
-        onEnter={() => setShowBackdrop(true)}
-        onExited={() => setShowBackdrop(false)}
-      >
-        <div className={classes.modalElement} ref={nodeRef}>
-          <ModalHeader title={title} onClose={onClose} variant={variant} />
-          <ModalBody>{children}</ModalBody>
-          <ModalFooter
-            isPending={isPending}
-            cancelText={cancelText}
-            confirmText={confirmText}
-            onCancel={onCancel}
-            onConfirm={onConfirm}
-            type={type}
-            variant={variant!}
-          />
-        </div>
-      </CSSTransition>
+      <Backdrop open={showBackdrop} portal={portal} zIndex={200} />
+      {modalElement}
     </>
   );
 };
