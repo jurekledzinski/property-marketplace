3879868862d7163cbf138a93bf2a5cdea50b537e
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-01 16:08:25 +0200

    Refactor ThemeProvider

--- store/theme/ThemeProvider.tsx
+++ store/theme/ThemeProvider.tsx
@@ -1,15 +1,20 @@
 'use client';
-import { ThemeContext } from './themeContext';
 import { ContextTheme, ThemeMode, ThemeProviderProps } from './types';
-import { useEffect, useMemo, useState } from 'react';
+import { ThemeContext } from './themeContext';
+import { useEffect, useMemo, useRef, useState } from 'react';
+
+import {
+  controlSetTimeoutDisable,
+  getInitialTheme,
+  setThemeAttributes,
+} from './utils';
 
 const ThemeProvider = ({ children }: ThemeProviderProps) => {
   const [theme, setTheme] = useState<ThemeMode>('light');
+  const timeoutId = useRef<NodeJS.Timeout>(null);
 
   useEffect(() => {
-    const stored = localStorage.getItem('mode');
-    const themeLocal = stored ? JSON.parse(stored) : 'light';
-    document.documentElement.setAttribute('data-theme', themeLocal);
+    const themeLocal = getInitialTheme();
     setTheme(themeLocal);
   }, []);
 
@@ -18,13 +23,19 @@ const ThemeProvider = ({ children }: ThemeProviderProps) => {
       mode: theme,
       onChange: (mode) => {
         setTheme(mode);
-        document.cookie = `mode=${mode}; path=/; max-age=31536000`;
-        localStorage.setItem('mode', JSON.stringify(mode));
-        document.documentElement.setAttribute('data-theme', mode);
+        setThemeAttributes(mode);
+        controlSetTimeoutDisable(timeoutId);
       },
     }),
     [theme]
   );
+
+  useEffect(() => {
+    const copyTimeoutId = timeoutId.current;
+    return () => {
+      if (copyTimeoutId) clearTimeout(copyTimeoutId);
+    };
+  }, []);
 
   return (
     <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
