fff98c18285735639b7e775190e192d9c889e284
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-02 22:54:53 +0200

    Work on custom legend for status chart in dashboard

--- components/pages/user-dashboard/components/adverts-status-chart/AdvertsStatusChart.tsx
+++ components/pages/user-dashboard/components/adverts-status-chart/AdvertsStatusChart.tsx
@@ -1,3 +1,4 @@
+import { Checkbox, CheckboxGroup } from '@/components/shared';
 import styles from '../../UserDashboard.module.css';
 import { AdvertsStatusChartProps } from './types';
 import { ArcElement, Chart as ChartJS, Legend, Tooltip } from 'chart.js';
@@ -6,15 +7,39 @@ import { Doughnut } from 'react-chartjs-2';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export const AdvertsStatusChart = ({
-  stats,
   options,
+  plugins,
+  stats,
+  stylesLegend = [],
+  toggleLegend,
 }: AdvertsStatusChartProps) => {
   return (
-    <div className={styles.statusChart}>
-      <Doughnut
-        data={stats ?? { labels: [], datasets: [] }}
-        options={options}
-      />
-    </div>
+    <>
+      <div className={styles.statusChart}>
+        <Doughnut
+          data={stats ?? { labels: [], datasets: [] }}
+          options={options}
+          plugins={plugins}
+        />
+      </div>
+      {/* <div className={styles.legendContainer} id="legend-container">
+        <CheckboxGroup orientation="column" spacing="tight">
+          {stylesLegend.map((item, index) => {
+            return (
+              <Checkbox
+                key={item.text}
+                id={item.text.toLowerCase()}
+                value={item.text.toLowerCase()}
+                size="size-xs"
+                checked={item.hidden}
+                onChange={() => toggleLegend(item.index || index)}
+              >
+                {item.text}
+              </Checkbox>
+            );
+          })}
+        </CheckboxGroup>
+      </div> */}
+    </>
   );
 };
