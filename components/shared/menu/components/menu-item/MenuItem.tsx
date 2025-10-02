f5bca3e66fcfe470acce542bf7d001a062e3e8e8
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-02 23:08:20 +0200

    Pass rest props to menu item

--- components/shared/menu/components/menu-item/MenuItem.tsx
+++ components/shared/menu/components/menu-item/MenuItem.tsx
@@ -2,6 +2,10 @@ import styles from '../../Menu.module.css';
 import { classNames } from '@/helpers';
 import { MenuItemProps } from './types';
 
-export const MenuItem = ({ children, className }: MenuItemProps) => {
-  return <li className={classNames(styles.menuItem, className)}>{children}</li>;
+export const MenuItem = ({ children, className, ...props }: MenuItemProps) => {
+  return (
+    <li className={classNames(styles.menuItem, className)} {...props}>
+      {children}
+    </li>
+  );
 };
