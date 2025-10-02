8462b3a531d0135c3fb4c58fb2b396c89812730a
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-02 22:56:43 +0200

    Add funciontity to control custom legend in doughnut chart

--- components/shared/chart-pie/hooks/chart-pie-options/types.ts
+++ components/shared/chart-pie/hooks/chart-pie-options/types.ts
@@ -1,6 +1,13 @@
 import { ChartData } from '@/components/shared/chart-bar';
+import { Plugin, ChartType } from 'chart.js';
+import type { ChartOptions, LegendItem } from 'chart.js';
 
 export type UseChartPieOptionsProps = {
   chartData?: ChartData;
   title?: string;
+};
+
+export type DoughnutPlugin = {
+  id: string;
+  afterUpdate: Plugin<ChartType, ChartOptions<'doughnut'>>['afterUpdate'];
 };
--- components/shared/chart-pie/hooks/chart-pie-options/useChartPieOptions.ts
+++ components/shared/chart-pie/hooks/chart-pie-options/useChartPieOptions.ts
@@ -1,26 +1,73 @@
 'use client';
-import { useMemo } from 'react';
-import type { ChartOptions } from 'chart.js';
+import { useMemo, useRef, useState } from 'react';
+import type { ChartOptions, LegendItem, Chart } from 'chart.js';
+import { DoughnutPlugin } from './types';
 
 export const useChartPieOptions = () => {
-  const options = useMemo<ChartOptions<'pie'>>(() => {
+  const [stylesLegend, setStylesLegend] = useState<LegendItem[]>();
+  const legendItems = useRef<LegendItem[]>([]);
+  const toggleVisibility = useRef<Chart['toggleDataVisibility']>(undefined);
+  const chartUpdate = useRef<Chart['update']>(undefined);
+  const isMounted = useRef(false);
+
+  const options = useMemo<ChartOptions<'doughnut'>>(() => {
     return {
+      type: 'doughnut',
       responsive: true,
       plugins: {
         legend: {
-          align: 'center',
-          position: 'bottom',
-          labels: {
-            boxHeight: 20,
-            boxWidth: 40,
-          },
+          display: false,
         },
         tooltip: {
           cornerRadius: 0,
+        },
+        htmlPlugin: {
+          containerID: 'legend-container',
         },
       },
     };
   }, []);
 
-  return options;
+  const objectPlugin: DoughnutPlugin = {
+    id: 'htmlPlugin',
+    afterUpdate: (chart) => {
+      if (
+        !chart ||
+        !chart.options ||
+        !chart.options.plugins ||
+        !chart.options.plugins.legend ||
+        !chart.options.plugins.legend.labels ||
+        !chart.options.plugins.legend.labels.generateLabels
+      )
+        return;
+
+      const items = chart.options.plugins.legend.labels.generateLabels(chart);
+
+      console.log('after', isMounted.current);
+
+      if (!isMounted.current) setStylesLegend(items);
+
+      legendItems.current = items;
+
+      if ('type' in chart.config && chart.config.type === 'doughnut') {
+        toggleVisibility.current = chart.toggleDataVisibility.bind(chart);
+        chartUpdate.current = chart.update.bind(chart);
+      }
+
+      isMounted.current = true;
+    },
+  };
+
+  const htmlLegendPlugin = [objectPlugin];
+
+  const toggleLegend = (index: number) => {
+    if (!toggleVisibility || !chartUpdate) return;
+    if (!chartUpdate?.current || !toggleVisibility.current) return;
+
+    isMounted.current = false;
+    toggleVisibility.current(index);
+    chartUpdate.current();
+  };
+
+  return { htmlLegendPlugin, options, stylesLegend, toggleLegend };
 };
