768b826d388abe377275ff319f0b3ecacb7a4c46
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-03 17:24:16 +0200

    Refactor alert helper function getClassNamesAlert

--- components/shared/alert/utils/classNames.ts
+++ components/shared/alert/utils/classNames.ts
@@ -1,27 +1,23 @@
 import styles from '../Alert.module.css';
-import { classNames, generateClassNames } from '@/helpers';
+import { generateClassNames } from '@/helpers';
 import { GetClassNamesAlert } from './types';
 
 export const getClassNamesAlert: GetClassNamesAlert = (params) => {
   const { color, fullWidth, radius, size, variant } = params;
   return {
-    alert: classNames(
-      styles.alert,
-      generateClassNames(styles, {
-        [`${color}`]: Boolean(color),
-        ['fullWidth']: Boolean(fullWidth),
-        [`${radius}`]: Boolean(radius),
-        [`${size}`]: Boolean(size),
-        [`${variant}`]: Boolean(variant),
-      })
-    ),
-    icon: classNames(
-      styles.icon,
-      generateClassNames(styles, {
-        [`${color}`]: Boolean(color),
-        [`${size}`]: Boolean(size),
-      })
-    ),
+    alert: generateClassNames(styles, {
+      alert: true,
+      fullWidth: Boolean(fullWidth),
+      [`${color}`]: Boolean(color),
+      [`${radius}`]: Boolean(radius),
+      [`${size}`]: Boolean(size),
+      [`${variant}`]: Boolean(variant),
+    }),
+    icon: generateClassNames(styles, {
+      icon: true,
+      [`${color}`]: Boolean(color),
+      [`${size}`]: Boolean(size),
+    }),
     message: styles.message,
   };
 };
