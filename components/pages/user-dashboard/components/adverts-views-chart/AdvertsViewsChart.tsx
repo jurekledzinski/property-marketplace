ebc74e1795a02391639e202bc70a92660d8716ae
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-03 17:22:25 +0200

    Add resize observer for chart bar to prevent flickering chart when open close drawer in dashboard

--- components/pages/user-dashboard/components/adverts-views-chart/AdvertsViewsChart.tsx
+++ components/pages/user-dashboard/components/adverts-views-chart/AdvertsViewsChart.tsx
@@ -2,9 +2,11 @@
 import styles from '../../UserDashboard.module.css';
 import { AdvertsViewsChartProps } from './types';
 import { Bar } from 'react-chartjs-2';
+import { useRef } from 'react';
+import { useResizeObserver } from '@/hooks';
 
 import {
-  Chart as ChartJS,
+  Chart,
   CategoryScale,
   LinearScale,
   BarElement,
@@ -13,25 +15,32 @@ import {
   Legend,
 } from 'chart.js';
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+Chart.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 export const AdvertsViewsChart = ({
   classChartContainer,
   options,
   views,
 }: AdvertsViewsChartProps) => {
+  const chartRef = useRef<Chart<'bar'>>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useResizeObserver({
+    onResize: () => {
+      if (chartRef.current) chartRef.current.resize();
+    },
+    ref: containerRef,
+  });
+
   return (
-    <div className={styles.viewsChart}>
+    <div ref={containerRef} className={styles.viewsChart}>
       <div className={classChartContainer}>
         <div id="chartBody" className={styles.chartBody}>
-          <Bar data={views ?? { labels: [], datasets: [] }} options={options} />
+          <Bar
+            ref={chartRef}
+            data={views ?? { labels: [], datasets: [] }}
+            options={options}
+          />
         </div>
       </div>
     </div>
