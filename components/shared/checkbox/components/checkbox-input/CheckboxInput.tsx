60c0a53d34f955796297062ff78887c5c2e5b27c
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-03 17:26:50 +0200

    Refactor CheckboxInput component

--- components/shared/checkbox/components/checkbox-input/CheckboxInput.tsx
+++ components/shared/checkbox/components/checkbox-input/CheckboxInput.tsx
@@ -1,26 +1,33 @@
 import { CheckboxProps } from '../../types';
 import { classNames } from '@/helpers';
 import { forwardRef } from 'react';
-import { getClassCheckbox } from '../../utils';
+import { getClassNamesCheckbox } from '../../utils';
 import { Icon } from '../icon';
 
 export const CheckboxInput = forwardRef<HTMLInputElement, CheckboxProps>(
   ({ className, color, disabled, readOnly, size, variant, ...props }, ref) => {
-    const classes = getClassCheckbox(color, disabled, readOnly, size, variant);
+    const classes = getClassNamesCheckbox({
+      className,
+      color,
+      disabled,
+      readOnly,
+      size,
+      variant,
+    });
 
     return (
       <>
         <input
           {...props}
           ref={ref}
-          className={classes.checkbox}
+          className={classes}
           disabled={disabled}
           id={props.id}
           readOnly={readOnly}
           type="checkbox"
           onKeyDown={(e) => readOnly && e.preventDefault()}
         />
-        <Icon className={classNames(classes.checkbox, className)} />
+        <Icon className={classNames(classes, className)} />
       </>
     );
   }
