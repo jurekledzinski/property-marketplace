12c51424c4b9467b21f7e214ea21348fae2e0009
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-05 15:51:16 +0200

    Rename fetch-api folder to api in lib folder

--- lib/index.ts
+++ lib/index.ts
@@ -2,6 +2,6 @@ export * from './actions';
 export * from './auth';
 export * from './constants';
 export * from './database';
-export * from './fetch-api';
+export * from './api';
 export * from './helpers';
 export * from './routes';
