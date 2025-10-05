194c6d52b3c1996f8ed63dbd8e2428566fb6bf04
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-05 15:11:42 +0200

    Add options object optionsFormatDate1 for format data

--- helpers/formatDate.ts
+++ helpers/formatDate.ts
@@ -15,3 +15,15 @@ export const formatDateLocalString = ({
 
   return new Intl.DateTimeFormat(locales, options).format(parsedDate);
 };
+
+// Monday, 29/09/2025, 17:09:20
+export const optionsFormatDate1: Intl.DateTimeFormatOptions = {
+  weekday: 'long',
+  day: '2-digit',
+  month: '2-digit',
+  year: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+  hour12: false,
+};
