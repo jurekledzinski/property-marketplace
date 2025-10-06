71def7f1a5d2177c61b4796bb1d1589c951cda80
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-06 20:51:17 +0200

    Create LocationSelect component used in LocationSection

---
+++ components/shared/property-form-sections/location-section/components/location-select/LocationSelect.tsx
@@ -0,0 +1,76 @@
+import { Controller } from 'react-hook-form';
+import { LocationSelectProps } from './types';
+import {
+  Field,
+  Label,
+  Loader,
+  LocationFields,
+  Message,
+  Select,
+  SelectPanel,
+  SelectScrollList,
+  SelectTrigger,
+} from '@/components';
+
+export const LocationSelect = <
+  T extends LocationFields,
+  V extends Record<string, unknown>
+>({
+  control,
+  nameField,
+  rulesField,
+  options,
+  placeHolder,
+  disabled,
+  errors,
+  data,
+  onSelect,
+  isLoadingCities,
+  isLoadingStates,
+  renderOption,
+  isLabel,
+  label,
+  onScrollEnd,
+}: LocationSelectProps<T, V>) => {
+  return (
+    <Field>
+      {isLabel && <Label>{label}</Label>}
+      <Controller
+        name={nameField}
+        control={control}
+        rules={rulesField}
+        render={({ field: { onChange, ...rest } }) => (
+          <Select
+            closeOnScroll
+            onChange={(key, value) => onSelect(key, onChange, value)}
+            options={options}
+            {...rest}
+          >
+            <SelectTrigger disabled={disabled} placeholder={placeHolder}>
+              {label === 'State' && disabled && isLoadingStates && (
+                <Loader position="element" />
+              )}
+              {label === 'City' && disabled && isLoadingCities && (
+                <Loader position="element" />
+              )}
+            </SelectTrigger>
+            <SelectPanel>
+              <SelectScrollList
+                isLoading={
+                  (label === 'State' && isLoadingStates) ||
+                  (label === 'City' && isLoadingCities)
+                }
+                onScrollEnd={onScrollEnd}
+              >
+                {data.map((country, index) => renderOption(country, index))}
+              </SelectScrollList>
+            </SelectPanel>
+          </Select>
+        )}
+      />
+      {errors[nameField] ? (
+        <Message variant="error">{errors[nameField].message}</Message>
+      ) : null}
+    </Field>
+  );
+};
