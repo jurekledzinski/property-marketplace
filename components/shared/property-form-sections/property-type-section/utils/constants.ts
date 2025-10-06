a7ca193358fd396e49d4e9d5ffd9949b0612985b
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-06 20:52:46 +0200

    Create constants file used in PropertyTypeSection

---
+++ components/shared/property-form-sections/property-type-section/utils/constants.ts
@@ -0,0 +1,20 @@
+export const propertyConditons = [
+  'New',
+  'Like New',
+  'Renovated',
+  'Good Condition',
+  'Needs Renovation',
+  'Under Construction',
+  'Old',
+  'Not Specified',
+];
+
+export const advertTypes = [
+  { key: 'house', value: 'House' },
+  { key: 'apartment', value: 'Apartment' },
+];
+
+export const advertStatutes = [
+  { key: 'sale', value: 'Sale' },
+  { key: 'rent', value: 'Rent' },
+];
