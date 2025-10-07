ca4c755a1c8e12fde76ccb6a8aac8ad074db10be
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-07 18:45:27 +0200

    Move usePathname to Header

--- components/shared/header/Header.tsx
+++ components/shared/header/Header.tsx
@@ -1,17 +1,24 @@
 'use client';
 import Link from 'next/link';
 import styles from './Header.module.css';
-import { AppBar, ButtonGroup, Container, Heading } from '@/components';
+import { AppBar, Button, ButtonGroup, Container, Heading } from '@/components';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { HeaderMenu, NavigationButtons } from './components';
 import { HeaderProps } from './types';
 import { memo } from 'react';
 import { showSuccessToast } from '@/utils-client';
 import { signOut } from 'next-auth/react';
 import { useDrawer, useTheme } from '@/store';
+import { usePathname } from 'next/navigation';
 
 export const Header = memo(({ session }: HeaderProps) => {
+  const pathname = usePathname();
   const { mode, onChange } = useTheme();
   const { onToggleFiltersPanel, onToggleMenuPanel } = useDrawer();
+
+  const segments = pathname.split('/').filter(Boolean);
+  const isDetails = segments[0] === 'adverts' && segments.length === 3;
+  console.log('segments', segments);
 
   return (
     <>
@@ -30,12 +37,22 @@ export const Header = memo(({ session }: HeaderProps) => {
               </Link>
             </Heading>
             <ButtonGroup spacing="normal">
+              {isDetails && (
+                <Button
+                  href="/"
+                  iconStart={[faArrowLeft]}
+                  label="Back"
+                  variant="minimal"
+                  size="size-xs"
+                />
+              )}
               <span className={styles.username}>{session?.name}</span>
               <NavigationButtons
                 mode={mode}
                 onChangeTheme={onChange}
                 onToggleFilters={onToggleFiltersPanel}
                 onToggleMenu={onToggleMenuPanel}
+                pathname={pathname}
               />
               <HeaderMenu
                 onLogout={async () => {
