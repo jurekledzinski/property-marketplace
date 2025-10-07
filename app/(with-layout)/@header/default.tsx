992fc403c1c9ebbf5bc41512b6840b884b16ba8b
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-07 17:28:25 +0200

    Fix not update user name in header when update profile

--- app/(with-layout)/@header/default.tsx
+++ app/(with-layout)/@header/default.tsx
@@ -1,10 +1,21 @@
 import { auth } from '@/auth';
+import { getUserProfilePage } from '@/services';
 import { Header as HeaderElement } from '@/components';
+import { headers } from 'next/headers';
 
 const Header = async () => {
   const session = await auth();
+  const headersData = await headers();
+  const user = await getUserProfilePage(headersData);
 
-  return <HeaderElement session={session?.user} />;
+  const authSession = session
+    ? {
+        ...session.user,
+        name: user ? user.name : session.user.name!,
+      }
+    : null;
+
+  return <HeaderElement session={authSession} />;
 };
 
 export default Header;
