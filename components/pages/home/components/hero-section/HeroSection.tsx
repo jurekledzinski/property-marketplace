180deb070d994c657e62cf00e24d4c75dba364db
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-11 20:50:49 +0200

    Conditionally show skeleton loading when image background loads in HeroSection

--- components/pages/home/components/hero-section/HeroSection.tsx
+++ components/pages/home/components/hero-section/HeroSection.tsx
@@ -1,9 +1,13 @@
 import styles from './HeroSection.module.css';
 import { Heading } from '@/components';
+import { useBackgroundImageLoaded } from '@/hooks';
+const src = '/images/house-hero-image.jpg';
 
 export const HeroSection = () => {
+  const isLoaded = useBackgroundImageLoaded(src);
+
   return (
-    <section className={styles.hero}>
+    <section className={isLoaded ? styles.hero : `${styles.hero} skeleton`}>
       <Heading className={styles.heading} level={2}>
         Explore Homes and Apartments
         <br /> for Rent or Sale
