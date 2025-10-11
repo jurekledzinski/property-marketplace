462b020b4cabcfa1fce115c867dbcd6f08b15fc5
Jurek Ledzinski <jurekledzinski@yahoo.pl>
2025-10-11 20:45:53 +0200

    Pass queryParams to Header component from header server component

--- app/(with-layout)/@header/page.tsx
+++ app/(with-layout)/@header/page.tsx
@@ -1,9 +1,12 @@
 import { auth } from '@/auth';
 import { getUserProfilePage } from '@/services';
 import { Header as HeaderElement } from '@/components';
+import { HeaderProps } from './types';
 import { headers } from 'next/headers';
 
-const Header = async () => {
+const Header = async ({ searchParams }: HeaderProps) => {
+  const data = await searchParams;
+  const queries = new URLSearchParams(Object.fromEntries(Object.entries(data)));
   const session = await auth();
   const headersData = await headers();
   const user = await getUserProfilePage(headersData);
@@ -15,7 +18,9 @@ const Header = async () => {
       }
     : null;
 
-  return <HeaderElement session={authSession} />;
+  return (
+    <HeaderElement queryParams={queries.toString()} session={authSession} />
+  );
 };
 
 export default Header;
